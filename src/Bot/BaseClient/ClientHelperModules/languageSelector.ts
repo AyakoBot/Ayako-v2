ec7d8c98b97a56195927717ff8c7359c5a686e52
Larsundso <Larsundso3@gmail.com>
2022-11-02 14:39:18 +0100

    changed accepted types

--- src/Bot/BaseClient/ClientHelperModules/languageSelector.ts
+++ src/Bot/BaseClient/ClientHelperModules/languageSelector.ts
@@ -2,7 +2,7 @@ import query from './query.js';
 import type DBT from '../../Typings/DataBaseTypings';
 import type CT from '../../Typings/CustomTypings';
 
-export default async (guildID: string | undefined | null): Promise<CT.Language> => {
+export default async (guildID: bigint | undefined | null | string): Promise<CT.Language> => {
   if (!guildID) {
     const { default: language } = await import(`../../Languages/en.json`, {
       assert: { type: 'json' },
@@ -11,9 +11,9 @@ export default async (guildID: string | undefined | null): Promise<CT.Language> 
     return language;
   }
 
-  const lan = await query('SELECT lan FROM guildsettings WHERE guildid = $1;', [guildID]).then(
-    (r: DBT.guildsettings[] | null) => (r ? r[0].lan : null),
-  );
+  const lan = await query('SELECT lan FROM guildsettings WHERE guildid = $1;', [
+    String(guildID),
+  ]).then((r: DBT.guildsettings[] | null) => (r ? r[0].lan : null));
 
   const { default: language } = await import(`../../Languages/${lan || 'en'}.json`, {
     assert: { type: 'json' },
