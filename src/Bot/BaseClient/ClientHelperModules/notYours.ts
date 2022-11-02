a1687dbed579b40e329444545c9c56035ad7bf60
Larsundso <Larsundso3@gmail.com>
2022-11-02 14:21:50 +0100

    added notYours module

---
+++ src/Bot/BaseClient/ClientHelperModules/notYours.ts
@@ -0,0 +1,21 @@
+import type DDeno from 'discordeno';
+import constants from '../Other/Constants.json' assert { type: 'json' };
+import reply from './replyCmd';
+
+export default (
+  interaction: DDeno.Interaction,
+  language: typeof import('../../Languages/en.json'),
+) => {
+  const embed: DDeno.Embed = {
+    type: 'rich',
+    author: {
+      name: language.error,
+      iconUrl: constants.standard.error,
+      url: constants.standard.invite,
+    },
+    color: constants.colors.warning,
+    description: language.errors.notYours,
+  };
+
+  reply(interaction, { data: { embeds: [embed] }, ephemeral: true, type: 4 });
+};
