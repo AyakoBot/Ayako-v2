2ceb3315a3ad8b7dae7d1eb3d1df17bd5f520d93
Larsundso <Larsundso3@gmail.com>
2022-12-04 19:21:11 +0100

    typing changes

--- src/Bot/BaseClient/ClientHelperModules/getJumpLink.ts
+++ src/Bot/BaseClient/ClientHelperModules/getJumpLink.ts
@@ -1,7 +1,9 @@
 import type * as DDeno from 'discordeno';
 import type CT from '../../Typings/CustomTypings';
 
-export default (msg: DDeno.Message | CT.Message) =>
+export default (
+  msg: DDeno.Message | CT.Message | { guildId?: bigint; channelId: bigint; id: bigint },
+) =>
   `https://discord.com/channels/${'guildId' in msg && msg.guildId ? msg.guildId : '@me'}/${
     msg.channelId
   }/${msg.id}`;
