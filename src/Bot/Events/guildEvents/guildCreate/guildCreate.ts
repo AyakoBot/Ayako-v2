c625a772211b0177a1b6373c89d255656d9344f7
Larsundso <Larsundso3@gmail.com>
2022-12-31 01:51:10 +0100

    guild create log

---
+++ src/Bot/Events/guildEvents/guildCreate/guildCreate.ts
@@ -0,0 +1,9 @@
+import type * as DDeno from 'discordeno';
+
+export default async (guild: DDeno.Guild) => {
+  const files: {
+    default: (g: DDeno.Guild) => void;
+  }[] = await Promise.all(['./log.js'].map((p) => import(p)));
+
+  files.forEach((f) => f.default(guild));
+};
