b790fe76a8be3a31efe7a22e2ed32e8637c0650d
Larsundso <Larsundso3@gmail.com>
2023-01-01 19:14:19 +0100

    support member update

--- src/Bot/BaseClient/ClientHelperModules/getAvatarURL.ts
+++ src/Bot/BaseClient/ClientHelperModules/getAvatarURL.ts
@@ -1,5 +1,8 @@
 import type DDeno from 'discordeno';
 import client from '../DDenoClient.js';
 
-export default (user: DDeno.User) =>
-  client.helpers.getAvatarURL(user.id, user.discriminator, { avatar: user.avatar, size: 4096 });
+export default (user: DDeno.User, member?: DDeno.Member) =>
+  client.helpers.getAvatarURL(user.id, user.discriminator, {
+    avatar: member ? member.avatar : user.avatar,
+    size: 4096,
+  });
