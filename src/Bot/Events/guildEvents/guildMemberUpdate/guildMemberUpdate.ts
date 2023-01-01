f8f5aaf6f05ff156929e8fa9cf9161f7436f7039
Larsundso <Larsundso3@gmail.com>
2023-01-01 22:35:12 +0100

    member update log

---
+++ src/Bot/Events/guildEvents/guildMemberUpdate/guildMemberUpdate.ts
@@ -0,0 +1,13 @@
+import client from 'Bot/BaseClient/DDenoClient.js';
+import type * as DDeno from 'discordeno';
+
+export default async (member: DDeno.Member, user: DDeno.User, oldMember: DDeno.Member) => {
+  const files: {
+    default: (m: DDeno.Member, t: DDeno.User, g: DDeno.Guild, o: DDeno.Member) => void;
+  }[] = await Promise.all(['./log.js'].map((p) => import(p)));
+
+  const guild = await client.cache.guilds.get(member.guildId);
+  if (!guild) return;
+
+  files.forEach((f) => f.default(member, user, guild, oldMember));
+};
