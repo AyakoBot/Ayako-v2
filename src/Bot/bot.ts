71d1406bcd630f97cbaf4f65437b76f8c170beb2
Larsundso <Larsundso3@gmail.com>
2023-01-05 21:39:21 +0100

    error log

--- src/Bot/bot.ts
+++ src/Bot/bot.ts
@@ -9,14 +9,19 @@ client.ch = ClientHelper;
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 rl.on('line', async (msg: string) => {
   if (msg === 'restart') process.exit();
-  // eslint-disable-next-line no-console
-  console.log(
-    msg.includes('await') || msg.includes('return')
-      ? // eslint-disable-next-line no-eval
-        await eval(`(async () => {${msg}})()`)
-      : // eslint-disable-next-line no-eval
-        eval(msg),
-  );
+  try {
+    // eslint-disable-next-line no-console
+    console.log(
+      msg.includes('await') || msg.includes('return')
+        ? // eslint-disable-next-line no-eval
+          await eval(`(async () => {${msg}})()`)
+        : // eslint-disable-next-line no-eval
+          eval(msg),
+    );
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
 });
 
 process.setMaxListeners(4);
