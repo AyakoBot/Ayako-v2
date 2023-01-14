fbd6c9149642e64cbc26d1b7bf4865aaf005cdff
Larsundso <Larsundso3@gmail.com>
2023-01-14 16:11:19 +0100

    caching on guild create and de-caching on guild leave

--- src/Bot/Events/guildEvents/guildCreate/guildCreate.ts
+++ src/Bot/Events/guildEvents/guildCreate/guildCreate.ts
@@ -3,7 +3,7 @@ import type * as DDeno from 'discordeno';
 export default async (guild: DDeno.Guild) => {
   const files: {
     default: (g: DDeno.Guild) => void;
-  }[] = await Promise.all(['./log.js'].map((p) => import(p)));
+  }[] = await Promise.all(['./log.js', './cache.js'].map((p) => import(p)));
 
   files.forEach((f) => f.default(guild));
 };
