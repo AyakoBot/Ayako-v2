5c03cae8ced97303ae190abfd985210900df2e21
Larsundso <Larsundso3@gmail.com>
2023-02-16 14:17:43 +0100

    getting into sharding

---
+++ src/bot.ts
@@ -0,0 +1,44 @@
+import readline from 'readline';
+import client from './BaseClient/Client.js';
+import * as ch from './BaseClient/ClientHelper.js';
+import cache from './BaseClient/ClientHelperModules/cache.js';
+
+client.ch = ch;
+client.cache = cache;
+client.setMaxListeners(client.events.length);
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.on('line', async (msg: string) => {
+  if (msg === 'restart') process.exit();
+  try {
+    // eslint-disable-next-line no-console
+    console.log(
+      msg.includes('await') || msg.includes('return')
+        ? // eslint-disable-next-line no-eval
+          await eval(`(async () => {${msg}})()`)
+        : // eslint-disable-next-line no-eval
+          eval(msg),
+    );
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+});
+
+process.setMaxListeners(4);
+process.on('unhandledRejection', async (error: string) => client.emit('unhandledRejection', error));
+process.on('uncaughtException', async (error: string) => client.emit('uncaughtException', error));
+process.on('promiseRejectionHandledWarning', (error: string) =>
+  client.emit('uncaughtException', error),
+);
+process.on('experimentalWarning', (error: string) => client.emit('uncaughtException', error));
+
+client.events.forEach(async (path) => {
+  const eventName = path.replace('.js', '').split(/\/+/).pop();
+  if (!eventName) return;
+
+  const eventHandler = (await import('./Events/baseEventHandler.js')).default;
+
+  if (eventName === 'ready') client.once(eventName, (...args) => eventHandler(eventName, args));
+  else client.on(eventName, (...args) => eventHandler(eventName, args));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,10 @@
-import readline from 'readline';
-import client from './BaseClient/Client.js';
-import * as ch from './BaseClient/ClientHelper.js';
-import cache from './BaseClient/ClientHelperModules/cache.js';
+import * as Discord from 'discord.js';
+import auth from './auth.json' assert { type: 'json' };
 
-client.ch = ch;
-client.cache = cache;
-client.setMaxListeners(client.events.length);
-
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-rl.on('line', async (msg: string) => {
-  if (msg === 'restart') process.exit();
-  try {
-    // eslint-disable-next-line no-console
-    console.log(
-      msg.includes('await') || msg.includes('return')
-        ? // eslint-disable-next-line no-eval
-          await eval(`(async () => {${msg}})()`)
-        : // eslint-disable-next-line no-eval
-          eval(msg),
-    );
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-  }
+const manager = new Discord.ShardingManager('./bot.js', {
+  token: auth.token,
 });
 
-process.setMaxListeners(4);
-process.on('unhandledRejection', async (error: string) => client.emit('unhandledRejection', error));
-process.on('uncaughtException', async (error: string) => client.emit('uncaughtException', error));
-process.on('promiseRejectionHandledWarning', (error: string) =>
-  client.emit('uncaughtException', error),
-);
-process.on('experimentalWarning', (error: string) => client.emit('uncaughtException', error));
+manager.on('shardCreate', (shard) => console.log(`[Shard Manager] Launched Shard ${shard.id}`));
 
-client.events.forEach(async (path) => {
-  const eventName = path.replace('.js', '').split(/\/+/).pop();
-  if (!eventName) return;
-
-  const eventHandler = (await import('./Events/baseEventHandler.js')).default;
-
-  if (eventName === 'ready') client.once(eventName, (...args) => eventHandler(eventName, args));
-  else client.on(eventName, (...args) => eventHandler(eventName, args));
-});
+manager.spawn();
