66dafdbf9f019e0b1dbe4775080b9e5e4924b471
Larsundso <Larsundso3@gmail.com>
2023-02-17 12:12:49 +0100

    de-evaled

--- src/BaseClient/ClientHelperModules/send.ts
+++ src/BaseClient/ClientHelperModules/send.ts
@@ -63,177 +63,163 @@ async function send(
     return sentMessages;
   }
 
+  if (payload.files?.length) timeout = undefined;
+  if (Number(payload.embeds?.length) > 1) timeout = undefined;
+  if (payload.components?.length) timeout = undefined;
+  if (payload.content?.length) timeout = undefined;
+
   const client = (await import('../Client.js')).default;
-  const response = await client.shard?.broadcastEval(
-    async (cl, context) => {
-      (cl as unknown as typeof Client).prototype;
+  const channel = !('name' in channels) ? client.channels.cache.get(channels.id) : channels;
+  if (!channel) return null;
 
-      const c = context.channels as
-        | Discord.TextBasedChannel
-        | {
-            id: string;
-            guildId: string;
-          };
+  if (!('send' in channel)) return null;
 
-      const { payload: p } = context;
-      let { timeout: t } = context;
+  if (
+    !payload.content?.length &&
+    !payload.embeds?.length &&
+    !payload.files?.length &&
+    !payload.components?.length
+  ) {
+    return null;
+  }
 
-      if (p.files?.length) t = undefined;
-      if (Number(p.embeds?.length) > 1) t = undefined;
-      if (p.components?.length) t = undefined;
-      if (p.content?.length) t = undefined;
+  const constants = (await import(`${process.cwd()}/BaseClient/Other/constants.js`)).default;
+  payload.embeds?.forEach((e) => {
+    if (e.author && !e.author.url) e.author.url = constants.standard.invite;
 
-      const channel = !('name' in c) ? cl.channels.cache.get(c.id) : c;
-      if (!channel) return null;
-
-      if (!('send' in channel)) return null;
-
-      if (!p.content?.length && !p.embeds?.length && !p.files?.length && !p.components?.length) {
-        return null;
+    e.fields?.forEach((f) => {
+      if (typeof f.inline !== 'boolean') {
+        f.inline = true;
       }
+    });
+  });
 
-      const constants = (await import(`${process.cwd()}/BaseClient/Other/constants.js`)).default;
-      p.embeds?.forEach((e) => {
-        if (e.author && !e.author.url) e.author.url = constants.standard.invite;
+  if (timeout && 'guild' in channel && payload.embeds?.length) {
+    combineMessages(channel, payload.embeds, timeout);
+    return null;
+  }
 
-        e.fields?.forEach((f) => {
-          if (typeof f.inline !== 'boolean') {
-            f.inline = true;
-          }
-        });
-      });
+  payload.embeds?.forEach((p) => {
+    p.fields?.forEach((p) => {
+      p.value?.length > 1024 ? console.log(p) : null;
+    });
+  });
 
-      const combineMessages = async (
-        channel:
-          | Discord.AnyThreadChannel<boolean>
-          | Discord.NewsChannel
-          | Discord.TextChannel
-          | Discord.VoiceChannel,
-        embeds: Discord.APIEmbed[],
-        timeout: number,
-      ) => {
-        const ch = (await import(
-          `${process.cwd()}/BaseClient/ClientHelper.js`
-        )) as typeof import('../ClientHelper.js');
+  const sentMessage = await channel.send(payload).catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log(payload.files);
+    console.log('send err', err);
+  });
 
-        let guildQueue = ch.channelQueue.get(channel.guildId);
-        if (!guildQueue) {
-          ch.channelQueue.set(channel.guildId, new Map());
-          guildQueue = ch.channelQueue.get(channel.guildId);
-        }
-
-        if (!guildQueue) {
-          ch.send(channel, { embeds });
-          return;
-        }
-
-        let channelQueues = guildQueue.get(channel.id);
-        if (!ch.channelQueue) {
-          guildQueue.set(channel.id, []);
-          channelQueues = guildQueue.get(channel.id);
-        }
-
-        if (!channelQueues) {
-          ch.send(channel, { embeds });
-          return;
-        }
-
-        if (
-          Number(channelQueues.length) + embeds.length > 10 ||
-          getEmbedCharLens([...channelQueues, ...embeds]) > 6000
-        ) {
-          ch.channelTimeout.get(channel.guildId)?.get(channel.id)?.cancel();
-          ch.send(channel, { embeds: channelQueues });
-          guildQueue.set(channel.id, []);
-          channelQueues = guildQueue.get(channel.id);
-        }
-
-        if (!channelQueues) {
-          ch.send(channel, { embeds });
-          return;
-        }
-
-        ch.channelQueue
-          .get(channel.guildId)
-          ?.get(channel.id)
-          ?.push(...embeds);
-
-        if (ch.channelTimeout.get(channel.guildId)?.get(channel.id)) return;
-
-        let timeoutGuild = ch.channelTimeout.get(channel.guildId);
-        if (!timeoutGuild) {
-          ch.channelTimeout.set(channel.guildId, new Map());
-          timeoutGuild = ch.channelTimeout.get(channel.guildId);
-        }
-
-        if (!timeoutGuild) {
-          ch.send(channel, { embeds });
-          return;
-        }
-
-        timeoutGuild.set(
-          channel.guildId,
-          Jobs.scheduleJob(new Date(Date.now() + timeout), () => {
-            const queuedEmbeds = ch.channelQueue.get(channel.guildId)?.get(channel.id) || [];
-            ch.send(channel, { embeds: queuedEmbeds });
-
-            ch.channelQueue.get(channel.guildId)?.delete(channel.id);
-            ch.channelTimeout.get(channel.guildId)?.delete(channel.id);
-
-            if (ch.channelQueue.get(channel.guildId)?.size === 0) {
-              ch.channelQueue.delete(channel.guildId);
-            }
-            if (ch.channelTimeout.get(channel.guildId)?.size === 0) {
-              ch.channelTimeout.delete(channel.guildId);
-            }
-          }),
-        );
-      };
-
-      const getEmbedCharLens = (embeds: Discord.APIEmbed[]) => {
-        let total = 0;
-        embeds.forEach((embed) => {
-          Object.values(embed).forEach((data) => {
-            if (typeof data === 'string') {
-              total += data.length;
-            }
-          });
-
-          for (let i = 0; i < (embed.fields ? embed.fields.length : 0); i += 1) {
-            const field = embed.fields ? embed.fields[i] : null;
-
-            if (!field) return;
-
-            if (typeof field.name === 'string') total += field.name.length;
-            if (typeof field.value === 'string') total += field.value.length;
-          }
-        });
-        return total;
-      };
-
-      if (t && 'guild' in channel && p.embeds?.length) {
-        combineMessages(channel, p.embeds, t);
-        return null;
-      }
-
-      p.embeds?.forEach((p) => {
-        p.fields?.forEach((p) => {
-          p.value?.length > 1024 ? console.log(p) : null;
-        });
-      });
-
-      const sentMessage = await channel.send(p as Discord.MessageCreateOptions).catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log(p.files);
-        console.log('send err', err);
-      });
-
-      return sentMessage;
-    },
-    { context: { channels, payload, command, timeout } },
-  );
-
-  return response?.filter((r): r is Discord.Message<boolean> => !!r);
+  return sentMessage;
 }
 
 export default send;
+
+const combineMessages = async (
+  channel:
+    | Discord.AnyThreadChannel<boolean>
+    | Discord.NewsChannel
+    | Discord.TextChannel
+    | Discord.VoiceChannel,
+  embeds: Discord.APIEmbed[],
+  timeout: number,
+) => {
+  const ch = (await import(
+    `${process.cwd()}/BaseClient/ClientHelper.js`
+  )) as typeof import('../ClientHelper.js');
+
+  let guildQueue = ch.channelQueue.get(channel.guildId);
+  if (!guildQueue) {
+    ch.channelQueue.set(channel.guildId, new Map());
+    guildQueue = ch.channelQueue.get(channel.guildId);
+  }
+
+  if (!guildQueue) {
+    ch.send(channel, { embeds });
+    return;
+  }
+
+  let channelQueues = guildQueue.get(channel.id);
+  if (!ch.channelQueue) {
+    guildQueue.set(channel.id, []);
+    channelQueues = guildQueue.get(channel.id);
+  }
+
+  if (!channelQueues) {
+    ch.send(channel, { embeds });
+    return;
+  }
+
+  if (
+    Number(channelQueues.length) + embeds.length > 10 ||
+    getEmbedCharLens([...channelQueues, ...embeds]) > 6000
+  ) {
+    ch.channelTimeout.get(channel.guildId)?.get(channel.id)?.cancel();
+    ch.send(channel, { embeds: channelQueues });
+    guildQueue.set(channel.id, []);
+    channelQueues = guildQueue.get(channel.id);
+  }
+
+  if (!channelQueues) {
+    ch.send(channel, { embeds });
+    return;
+  }
+
+  ch.channelQueue
+    .get(channel.guildId)
+    ?.get(channel.id)
+    ?.push(...embeds);
+
+  if (ch.channelTimeout.get(channel.guildId)?.get(channel.id)) return;
+
+  let timeoutGuild = ch.channelTimeout.get(channel.guildId);
+  if (!timeoutGuild) {
+    ch.channelTimeout.set(channel.guildId, new Map());
+    timeoutGuild = ch.channelTimeout.get(channel.guildId);
+  }
+
+  if (!timeoutGuild) {
+    ch.send(channel, { embeds });
+    return;
+  }
+
+  timeoutGuild.set(
+    channel.guildId,
+    Jobs.scheduleJob(new Date(Date.now() + timeout), () => {
+      const queuedEmbeds = ch.channelQueue.get(channel.guildId)?.get(channel.id) || [];
+      ch.send(channel, { embeds: queuedEmbeds });
+
+      ch.channelQueue.get(channel.guildId)?.delete(channel.id);
+      ch.channelTimeout.get(channel.guildId)?.delete(channel.id);
+
+      if (ch.channelQueue.get(channel.guildId)?.size === 0) {
+        ch.channelQueue.delete(channel.guildId);
+      }
+      if (ch.channelTimeout.get(channel.guildId)?.size === 0) {
+        ch.channelTimeout.delete(channel.guildId);
+      }
+    }),
+  );
+};
+
+const getEmbedCharLens = (embeds: Discord.APIEmbed[]) => {
+  let total = 0;
+  embeds.forEach((embed) => {
+    Object.values(embed).forEach((data) => {
+      if (typeof data === 'string') {
+        total += data.length;
+      }
+    });
+
+    for (let i = 0; i < (embed.fields ? embed.fields.length : 0); i += 1) {
+      const field = embed.fields ? embed.fields[i] : null;
+
+      if (!field) return;
+
+      if (typeof field.name === 'string') total += field.name.length;
+      if (typeof field.value === 'string') total += field.value.length;
+    }
+  });
+  return total;
+};
