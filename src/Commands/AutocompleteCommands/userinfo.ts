859b2ab5ddc7739e463e2548120950141a46c614
Larsundso <Larsundso3@gmail.com>
2023-02-19 03:38:02 +0100

    dont query users

--- src/Commands/AutocompleteCommands/userinfo.ts
+++ src/Commands/AutocompleteCommands/userinfo.ts
@@ -3,14 +3,9 @@ import * as ch from '../../BaseClient/ClientHelper.js';
 
 export default async (cmd: Discord.AutocompleteInteraction) => {
   const value = cmd.options.get('user-name', false)?.value as string;
-  const possibleUsers: Discord.User[] = [];
-
   if (value?.length < 3) return;
 
-  if (cmd.guild) {
-    possibleUsers.push(...(await cmd.guild?.members.fetch({ query: value })).map((m) => m.user));
-  }
-  possibleUsers.push(...(await ch.findUserByName(value)));
+  const possibleUsers = await ch.findUserByName(value);
 
   cmd.respond(
     [...new Set(possibleUsers.map((u) => u.id))]
