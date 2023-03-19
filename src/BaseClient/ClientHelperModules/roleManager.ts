ffe965af33d7454190c81d6ea62d94d3cc7923ff
Larsundso <Larsundso3@gmail.com>
2023-03-19 22:55:34 +0100

    debugged

--- src/BaseClient/ClientHelperModules/roleManager.ts
+++ src/BaseClient/ClientHelperModules/roleManager.ts
@@ -1,7 +1,7 @@
 import * as Jobs from 'node-schedule';
 import * as Discord from 'discord.js';
 
-const MemberCache: {
+const MemberCaches: {
   member: Discord.GuildMember;
   addRoles?: string[];
   removeRoles?: string[];
@@ -12,7 +12,7 @@ const MemberCache: {
 
 const GuildCache: Map<
   string,
-  { job: Jobs.Job; members: typeof MemberCache; guild: Discord.Guild }
+  { job: Jobs.Job; members: typeof MemberCaches; guild: Discord.Guild }
 > = new Map();
 
 const roleManager = {
@@ -34,9 +34,6 @@ const handleRoleUpdate = async (
   const { me } = member.guild.members;
   if (!me) return;
 
-  if (!member.manageable) return;
-  if (!new Discord.PermissionsBitField(me.permissions).has(268435456n)) return;
-
   const roleGuild = GuildCache.get(member.guild.id);
   if (!roleGuild) {
     GuildCache.set(member.guild.id, {
@@ -44,10 +41,11 @@ const handleRoleUpdate = async (
       members: [{ member, [type]: roles, reason, prio, added: Date.now() }],
       guild: member.guild,
     });
+
     return;
   }
 
-  const existingEntry = MemberCache[MemberCache.findIndex((c) => c.member.id === member.id)];
+  const existingEntry = MemberCaches[MemberCaches.findIndex((c) => c.member.id === member.id)];
   if (existingEntry) {
     existingEntry[type] = existingEntry[type]?.length
       ? [...new Set([...(existingEntry[type] as string[]), ...roles])]
@@ -56,55 +54,62 @@ const handleRoleUpdate = async (
     return;
   }
 
-  MemberCache.push({ member, [type]: roles, prio, reason, added: Date.now() });
+  MemberCaches.push({ member, [type]: roles, prio, reason, added: Date.now() });
 };
 
 export default roleManager;
 
 const runJob = async (guild: Discord.Guild) => {
-  const memberCache = GuildCache.get(guild.id);
-  if (!memberCache) return;
+  const endJob = (memberCaches: {
+    job: Jobs.Job;
+    members: typeof MemberCaches;
+    guild: Discord.Guild;
+  }) => {
+    const index = memberCaches.members.findIndex((m) => m.member.id === memberCache.member.id);
+    memberCaches.members.splice(index, 1);
 
-  const prioSort = memberCache?.members.sort((a, b) => a.prio - b.prio);
-  const highestPrio = prioSort[0]?.prio;
-  if (!highestPrio) return;
+    if (!memberCaches.members.length) {
+      memberCaches.job.cancel();
+      GuildCache.delete(guild.id);
+    }
+  };
 
-  const prioFilter = memberCache?.members.filter((m) => m.prio === highestPrio);
-  const dateFilter = prioFilter.sort((a, b) => b.added - a.added);
-  const memberData = dateFilter[0];
-  const roles = memberData.addRoles?.length
-    ? [...memberData.member.roles.cache.map((r) => r), ...memberData.addRoles]
-    : memberData.member.roles.cache.map((r) => r);
+  const memberCaches = GuildCache.get(guild.id);
+  if (!memberCaches) return;
+
   const { me } = guild.members;
-  const clientHighestRole = me?.roles.cache
-    .sort((a, b) => b.position - a.position)
-    .map((r) => r)
-    .shift();
+  if (!me?.permissions.has(268435456n)) return endJob(memberCaches);
 
-  if (!clientHighestRole) return;
+  const prioSort = memberCaches?.members.sort((a, b) => a.prio - b.prio);
+  const highestPrio = prioSort[0]?.prio;
+  const prioFilter = memberCaches?.members.filter((m) => m.prio === highestPrio);
+  const dateFilter = prioFilter.sort((a, b) => b.added - a.added);
+  const memberCache = dateFilter[0];
+  const roles = memberCache.addRoles?.length
+    ? [...memberCache.member.roles.cache.map((r) => r), ...memberCache.addRoles]
+    : memberCache.member.roles.cache.map((r) => r);
+
+  const clientHighestRole = me?.roles.highest;
+  if (!clientHighestRole) return endJob(memberCaches);
 
   const editedRoles = roles.filter((r) => {
-    const role = typeof r === 'string' ? memberCache.guild.roles.cache.get(r) : r;
+    const role = typeof r === 'string' ? memberCaches.guild.roles.cache.get(r) : r;
 
     if (!role) return false;
-    if (memberData.removeRoles?.includes(typeof r === 'string' ? r : r.id)) return false;
+    if (role.id === guild.id) return false;
+    if (memberCache.removeRoles?.includes(typeof r === 'string' ? r : r.id)) return false;
     if (clientHighestRole.position < role.position) return false;
 
     return true;
   });
 
-  const roleAdd = await memberData.member
+  if (!editedRoles.length) return endJob(memberCaches);
+
+  memberCache.member
     .edit({
       roles: editedRoles,
     })
-    .catch(() => null);
-  if (!roleAdd) return;
+    .catch(console.error);
 
-  const index = memberCache.members.findIndex((m) => m.member.id === memberData.member.id);
-  memberCache.members.splice(index, 1);
-
-  if (!memberCache.members.length) {
-    memberCache.job.cancel();
-    GuildCache.delete(guild.id);
-  }
+  endJob(memberCaches);
 };
