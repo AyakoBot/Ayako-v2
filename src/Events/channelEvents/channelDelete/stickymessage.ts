f3ffead7eaece945d08c38bc7abec825f8fc9138
Larsundso <Larsundso3@gmail.com>
2023-05-07 10:08:37 +0200

    wrong table name

--- src/Events/channelEvents/channelDelete/stickymessage.ts
+++ src/Events/channelEvents/channelDelete/stickymessage.ts
@@ -2,4 +2,4 @@ import * as Discord from 'discord.js';
 import * as ch from '../../../BaseClient/ClientHelper.js';
 
 export default (channel: Discord.Channel) =>
- ch.query(`DELETE FROM stickymessage WHERE channelid = $1;`, [channel.id]);
+ ch.query(`DELETE FROM stickymessages WHERE channelid = $1;`, [channel.id]);
