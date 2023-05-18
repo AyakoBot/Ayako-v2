39b31d4f8182b534e2c7971b73de444ab1c81208
Larsundso <Larsundso3@gmail.com>
2023-05-18 21:08:17 +0200

    improvements

--- src/Events/readyEvents/startupTasks/slashCommandInitializer.ts
+++ src/Events/readyEvents/startupTasks/slashCommandInitializer.ts
@@ -2,18 +2,6 @@ import commands from './SlashCommands.js';
 import client from '../../../BaseClient/Client.js';
 
 export default async () => {
- await client.application?.commands.fetch();
-
- Object.values(commands.public).forEach((command) => {
-  const exists = client.application?.commands.cache.find((c) => c.name === command.name);
-
-  if (exists) client.application?.commands.edit(exists, command);
-  else client.application?.commands.create(command);
- });
-
- client.application?.commands.cache.forEach((command) => {
-  const exists = Object.values(commands.public).find((c) => command.name === c.name);
-
-  if (!exists) client.application?.commands.delete(command.id);
- });
+ const createCommands = Object.values(commands.public);
+ client.application?.commands.set(createCommands);
 };
