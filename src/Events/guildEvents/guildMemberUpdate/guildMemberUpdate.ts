89300a3b7974f5207bca6bbb0c372988e6a605ed
Larsundso <Larsundso3@gmail.com>
2023-07-17 18:21:15 +0200

    remove userbot detector

--- src/Events/guildEvents/guildMemberUpdate/guildMemberUpdate.ts
+++ src/Events/guildEvents/guildMemberUpdate/guildMemberUpdate.ts
@@ -1,10 +1,8 @@
 import type * as Discord from 'discord.js';
 import log from './log.js';
 import separator from './separator.js';
-import userBotDetector from './userBotDetector.js';
 
 export default async (oldMember: Discord.GuildMember, member: Discord.GuildMember) => {
  log(oldMember, member);
  separator(oldMember, member);
- userBotDetector(oldMember, member);
 };
