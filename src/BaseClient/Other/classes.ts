d2bbeb23a9d529b57a75069ef9474291891f12d1
Larsundso <Larsundso3@gmail.com>
2023-08-22 19:08:40 +0200

    custom classes

---
+++ src/BaseClient/Other/classes.ts
@@ -0,0 +1,78 @@
+import * as Discord from 'discord.js';
+import * as RawData from 'discord.js/typings/rawDataTypes.js';
+
+export default undefined;
+
+// @ts-ignore
+export class GuildBan extends Discord.GuildBan {
+ constructor(client: Discord.Client<true>, data: RawData.RawGuildBanData, guild: Discord.Guild) {
+  super(client, data, guild);
+ }
+}
+
+// @ts-ignore
+export class Invite extends Discord.Invite {
+ constructor(client: Discord.Client<true>, data: RawData.RawInviteData) {
+  super(client, data);
+ }
+}
+
+// @ts-ignore
+export class GuildMember extends Discord.GuildMember {
+ constructor(
+  client: Discord.Client<true>,
+  data:
+   | Discord.APIGuildMember
+   | Discord.APIInteractionGuildMember
+   | Discord.APIInteractionDataResolvedGuildMember
+   | Discord.GatewayGuildMemberAddDispatchData
+   | Discord.GatewayGuildMemberUpdateDispatchData
+   | Required<Discord.RESTPatchAPICurrentGuildMemberNicknameJSONBody>
+   | { user: { id: Discord.Snowflake } },
+  guild: Discord.Guild,
+ ) {
+  super(client, data, guild);
+ }
+}
+
+// @ts-ignore
+export class Webhook extends Discord.Webhook {
+ constructor(client: Discord.Client<true>, data: RawData.RawWebhookData) {
+  super(client, data);
+ }
+}
+
+// @ts-ignore
+export class Message extends Discord.Message {
+ constructor(client: Discord.Client<true>, data: RawData.RawMessageData) {
+  super(client, data);
+ }
+}
+
+// @ts-ignore
+export class GuildAuditLogs extends Discord.GuildAuditLogs {
+ constructor(guild: Discord.Guild, data: RawData.RawGuildAuditLogData) {
+  super(guild, data);
+ }
+}
+
+// @ts-ignore
+export class Integration extends Discord.Integration {
+ constructor(client: Discord.Client<true>, data: RawData.RawIntegrationData, guild: Discord.Guild) {
+  super(client, data, guild);
+ }
+}
+
+// @ts-ignore
+export class GuildOnboarding extends Discord.GuildOnboarding {
+ constructor(client: Discord.Client<true>, data: Discord.RESTGetAPIGuildOnboardingResult) {
+  super(client, data);
+ }
+}
+
+// @ts-ignore
+export class WelcomeScreen extends Discord.WelcomeScreen {
+ constructor(guild: Discord.Guild, data: RawData.RawWelcomeScreenData) {
+  super(guild, data);
+ }
+}
