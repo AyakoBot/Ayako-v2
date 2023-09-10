61a410229451b1fc698998c54c1521a256644bc2
Larsundso <Larsundso3@gmail.com>
2023-09-10 16:57:22 +0200

    import fixes

--- src/Events/channelEvents/channelPinsUpdate/channelPinsUpdate.ts
+++ src/Events/channelEvents/channelPinsUpdate/channelPinsUpdate.ts
@@ -1,8 +1,17 @@
 import type * as Discord from 'discord.js';
 import * as ch from '../../../BaseClient/ClientHelper.js';
-import client from '../../../BaseClient/Client.js';
+import channelPinsCreate from './channelPinsCreate/channelPinsCreate.js';
+import channelPinsDelete from './channelPinsDelete/channelPinsDelete.js';
 
-export default async (channel: Discord.TextBasedChannel, date: Date) => {
+export default async (
+ channel:
+  | Discord.NewsChannel
+  | Discord.TextChannel
+  | Discord.PrivateThreadChannel
+  | Discord.PublicThreadChannel
+  | Discord.VoiceChannel,
+ date: Date,
+) => {
  if (channel.isDMBased()) return;
 
  const newPins = await channel.messages.fetchPinned();
@@ -12,6 +21,6 @@ export default async (channel: Discord.TextBasedChannel, date: Date) => {
   ([, p]) => p,
  ).filter((p) => !newPins.has(p.id));
 
- addedPins.forEach(async (p) => client.emit('channelPinsCreate', p, channel, date));
- removedPins.forEach(async (p) => client.emit('channelPinsDelete', p, channel, date));
+ addedPins.forEach(async (p) => channelPinsCreate(p, channel, date));
+ removedPins.forEach(async (p) => channelPinsDelete(p, channel));
 };
