55efbe34ba2209b9097dd190751eade98c64f889
Larsundso <Larsundso3@gmail.com>
2023-09-10 17:24:17 +0200

    de-modified events

--- src/BaseClient/ClientHelperModules/getEvents.ts
+++ src/BaseClient/ClientHelperModules/getEvents.ts
@@ -1,85 +1,7 @@
 import { glob } from 'glob';
 import * as Discord from 'discord.js';
 
-type EventValues = `${Discord.Events}`;
-
-export const gatewayEvents: (EventValues | string)[] = [
- 'applicationCommandPermissionsUpdate',
- 'autoModerationActionExecution',
- 'autoModerationRuleCreate',
- 'autoModerationRuleDelete',
- 'autoModerationRuleUpdate',
- 'channelCreate',
- 'channelDelete',
- 'channelPinsCreate',
- 'channelPinsDelete',
- 'channelPinsUpdate',
- 'channelUpdate',
- 'debug',
- 'emojiCreate',
- 'emojiDelete',
- 'emojiUpdate',
- 'error',
- 'guildBanAdd',
- 'guildBanRemove',
- 'guildCreate',
- 'guildDelete',
- 'guildIntegrationsCreates',
- 'guildIntegrationsDeletes',
- 'guildIntegrationsUpdate',
- 'guildIntegrationsUpdates',
- 'guildMemberAdd',
- 'guildMemberPrune',
- 'guildMemberRemove',
- 'guildMemberUpdate',
- 'guildScheduledEventCreate',
- 'guildScheduledEventDelete',
- 'guildScheduledEventUpdate',
- 'guildScheduledEventUserAdd',
- 'guildScheduledEventUserRemove',
- 'guildAuditLogEntryCreate',
- 'guildUpdate',
- 'interactionCreate',
- 'inviteCreate',
- 'inviteDelete',
- 'messageCreate',
- 'messageDelete',
- 'messageDeleteBulk',
- 'messageReactionAdd',
- 'messageReactionRemove',
- 'messageReactionRemoveAll',
- 'messageReactionRemoveEmoji',
- 'messageUpdate',
- 'modBaseEvent',
- 'modSourceHandler',
- 'ready',
- 'roleCreate',
- 'roleDelete',
- 'roleUpdate',
- 'stageInstanceCreate',
- 'stageInstanceDelete',
- 'stageInstanceUpdate',
- 'stickerCreate',
- 'stickerDelete',
- 'stickerUpdate',
- 'threadCreate',
- 'threadDelete',
- 'threadMembersUpdate',
- 'threadUpdate',
- 'typingStart',
- 'uncaughtException',
- 'unhandledRejection',
- 'userUpdate',
- 'voiceStateCreates',
- 'voiceStateDeletes',
- 'voiceStateUpdate',
- 'voiceStateUpdates',
- 'webhooksCreate',
- 'webhooksDelete',
- 'webhooksUpdate',
-];
-
-export default async (): Promise<typeof gatewayEvents> => {
+export default async () => {
  const events = await glob(`${process.cwd()}/Events/**/*`);
 
  const filteredEvents = events
@@ -88,7 +10,7 @@ export default async (): Promise<typeof gatewayEvents> => {
    const eventName = path.replace('.js', '').split(/\/+/).pop();
 
    if (!eventName) return false;
-   if (!gatewayEvents.includes(eventName)) return false;
+   if (!Object.values(Discord.Events).find(eventName as never)) return false;
    return true;
   });
 
