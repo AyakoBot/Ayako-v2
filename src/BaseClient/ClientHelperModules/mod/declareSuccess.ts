9bd3738cc89ce2cb189e420b4b3710de91c5007f
Larsundso <Larsundso3@gmail.com>
2023-09-15 11:39:21 +0200

    correct success declaration

--- src/BaseClient/ClientHelperModules/mod/declareSuccess.ts
+++ src/BaseClient/ClientHelperModules/mod/declareSuccess.ts
@@ -4,6 +4,7 @@ import * as CT from '../../../Typings/CustomTypings.js';
 
 import constants from '../../Other/constants.js';
 import { request } from '../requestHandler.js';
+import send from '../send.js';
 
 export default async <T extends CT.ModTypes>(
  cmd: ModTypes.CmdType,
@@ -24,6 +25,13 @@ export default async <T extends CT.ModTypes>(
   embeds: [embed],
  };
 
- if (!(cmd instanceof Discord.Message) && cmd) cmd.editReply(payload);
- else if (message instanceof Discord.Message) request.channels.editMsg(message, payload);
+ if ((cmd instanceof Discord.Message || !cmd) && message instanceof Discord.Message) {
+  request.channels.editMsg(message, payload);
+  return;
+ }
+ if (cmd instanceof Discord.Message) return;
+ if (!cmd) return;
+
+ cmd.deleteReply();
+ send({ id: cmd.channelId, guildId: cmd.guildId }, payload);
 };
