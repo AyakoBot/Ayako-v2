8f4c4fca47cb73a28028e53fbbfb4ae7e8fe422c
Larsundso <Larsundso3@gmail.com>
2023-09-17 19:14:06 +0200

    properly cache and un-cache deleteThreads

--- src/BaseClient/ClientHelperModules/cache/bot/deleteThreads.ts
+++ src/BaseClient/ClientHelperModules/cache/bot/deleteThreads.ts
@@ -1,4 +1,5 @@
 import * as Jobs from 'node-schedule';
+import DataBase from '../../../DataBase.js';
 
 export interface DeleteThreads {
  set: (job: Jobs.Job, guildId: string, channelId: string) => void;
@@ -13,6 +14,16 @@ const self: DeleteThreads = {
 
   if (!self.cache.get(guildId)) self.cache.set(guildId, new Map());
   self.cache.get(guildId)?.set(channelId, job);
+
+  DataBase.deletethreads
+   .create({
+    data: {
+     guildid: guildId,
+     channelid: channelId,
+     deletetime: job.nextInvocation().getTime(),
+    },
+   })
+   .then();
  },
  delete: (guildId, channelId) => {
   const cached = self.cache.get(guildId)?.get(channelId);
@@ -20,6 +31,15 @@ const self: DeleteThreads = {
 
   if (self.cache.get(guildId)?.size === 1) self.cache.delete(guildId);
   else self.cache.get(guildId)?.delete(channelId);
+
+  DataBase.deletethreads
+   .delete({
+    where: {
+     guildid: guildId,
+     channelid: channelId,
+    },
+   })
+   .then();
  },
  cache: new Map(),
 };
--- src/BaseClient/ClientHelperModules/mod/deleteThread.ts
+++ src/BaseClient/ClientHelperModules/mod/deleteThread.ts
@@ -2,20 +2,11 @@ import * as Discord from 'discord.js';
 
 import { request } from '../requestHandler.js';
 import cache from '../cache.js';
-import DataBase from '../../DataBase.js';
 
 export default async (guild: Discord.Guild, threadId: string) => {
  cache.deleteThreads.delete(guild.id, threadId);
- DataBase.deletethreads
-  .delete({
-   where: {
-    guildid: guild.id,
-    channelid: threadId,
-   },
-  })
-  .then();
 
- if (!guild.rulesChannel) return;
+ if (!guild.rulesChannelId) return;
  const rawThread = await request.channels.get(guild, threadId);
  if ('message' in rawThread) return;
  request.channels.delete(guild, rawThread.id);
