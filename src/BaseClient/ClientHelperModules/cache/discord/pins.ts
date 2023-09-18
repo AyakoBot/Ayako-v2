dec2bfabb330499fa927c4929f7b0908e8acef38
Larsundso <Larsundso3@gmail.com>
2023-09-18 22:58:29 +0200

    return if not a channel with pins

--- src/BaseClient/ClientHelperModules/cache/discord/pins.ts
+++ src/BaseClient/ClientHelperModules/cache/discord/pins.ts
@@ -19,6 +19,8 @@ const self: Pins = {
   const cached = self.cache.get(channel.guildId)?.get(channel.id)?.get(id);
   if (cached) return cached;
 
+  if (!('lastPinTimestamp' in channel)) return undefined;
+
   // eslint-disable-next-line import/no-cycle
   const requestHandler = (await import('../../requestHandler.js')).request;
   const me = await getBotMemberFromGuild(channel.guild);
