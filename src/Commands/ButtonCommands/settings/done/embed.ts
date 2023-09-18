5c60c09155419137208b45c95c1d82e8782c8187
Larsundso <Larsundso3@gmail.com>
2023-09-18 22:19:15 +0200

    missing editors

---
+++ src/Commands/ButtonCommands/settings/done/embed.ts
@@ -0,0 +1,66 @@
+import * as Discord from 'discord.js';
+import * as ch from '../../../../BaseClient/ClientHelper.js';
+import * as CT from '../../../../Typings/CustomTypings.js';
+
+export default async (cmd: Discord.ButtonInteraction, args: string[]) => {
+ if (!cmd.inCachedGuild()) return;
+
+ const settingName = args.shift() as keyof CT.Language['slashCommands']['settings']['categories'];
+ if (!settingName) return;
+
+ const fieldName = args.shift();
+ if (!fieldName) return;
+
+ const getUniquetimestamp = () => {
+  const arg = args.shift();
+  if (arg) return Number(arg);
+  return undefined;
+ };
+ const uniquetimestamp = getUniquetimestamp();
+
+ const currentSetting = await ch.settingsHelpers.changeHelpers.get(
+  settingName,
+  cmd.guildId,
+  uniquetimestamp,
+ );
+
+ const language = await ch.languageSelector(cmd.guildId);
+ const embedID = cmd.message.embeds[0].description;
+
+ const updatedSetting = await ch.settingsHelpers.changeHelpers.getAndInsert(
+  settingName,
+  fieldName,
+  cmd.guildId,
+  embedID,
+  uniquetimestamp,
+ );
+
+ ch.settingsHelpers.updateLog(
+  { [fieldName]: currentSetting?.[fieldName as keyof typeof currentSetting] },
+  { [fieldName]: updatedSetting?.[fieldName as keyof typeof updatedSetting] },
+  fieldName as CT.Argument<(typeof ch)['settingsHelpers']['updateLog'], 2>,
+  settingName,
+  uniquetimestamp,
+  cmd.guild,
+  language,
+  language.slashCommands.settings.categories[settingName],
+ );
+
+ const settingsFile = await ch.settingsHelpers.getSettingsFile(settingName, cmd.guild);
+ if (!settingsFile) return;
+
+ cmd.update({
+  embeds: await settingsFile.getEmbeds(
+   ch.settingsHelpers.embedParsers,
+   updatedSetting,
+   language,
+   language.slashCommands.settings.categories[settingName],
+   cmd.guild,
+  ),
+  components: await settingsFile.getComponents(
+   ch.settingsHelpers.buttonParsers,
+   updatedSetting,
+   language,
+  ),
+ });
+};
---
+++ src/Commands/SelectCommands/StringSelect/settings/embed.ts
@@ -0,0 +1,28 @@
+import type * as Discord from 'discord.js';
+import * as ch from '../../../../BaseClient/ClientHelper.js';
+import type CT from '../../../../Typings/CustomTypings.js';
+
+export default async (cmd: Discord.StringSelectMenuInteraction, args: string[]) => {
+ if (!cmd.inCachedGuild()) return;
+
+ const fieldName = args.shift();
+ if (!fieldName) return;
+
+ const settingName = args.shift() as keyof CT.Language['slashCommands']['settings']['categories'];
+ if (!settingName) return;
+
+ const language = await ch.languageSelector(cmd.guildId);
+
+ cmd.update({
+  embeds: [
+   await ch.settingsHelpers.changeHelpers.changeEmbed(
+    language,
+    settingName,
+    fieldName,
+    cmd.values,
+    'embed',
+    cmd.guild,
+   ),
+  ],
+ });
+};
