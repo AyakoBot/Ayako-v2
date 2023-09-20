6b91efd89d1100645305fcac0a4bd3845a258611
Larsundso <Larsundso3@gmail.com>
2023-09-20 21:53:42 +0200

    better perms checking

--- src/BaseClient/ClientHelperModules/isManageable.ts
+++ src/BaseClient/ClientHelperModules/isManageable.ts
@@ -1,14 +1,21 @@
 import * as Discord from 'discord.js';
 
 export default (
- target: Discord.GuildMember | Discord.GuildBasedChannel,
+ target: Discord.GuildMember | Discord.GuildBasedChannel | Discord.Role,
  executor: Discord.GuildMember,
 ) => {
  if (target instanceof Discord.GuildMember) return member(target, executor);
+ if (target instanceof Discord.Role) return role(target, executor);
  return channel(target, executor);
 };
 
 const member = (target: Discord.GuildMember, executor: Discord.GuildMember) => {
+ if (
+  !target.permissions.has(Discord.PermissionFlagsBits.ManageRoles, true) ||
+  !target.permissions.has(Discord.PermissionFlagsBits.ModerateMembers, true)
+ ) {
+  return false;
+ }
  if (target.user.id === target.guild.ownerId) return false;
  if (target.user.id === executor.user.id) return false;
  if (executor.user.id === executor.guild.ownerId) return true;
@@ -17,3 +24,9 @@ const member = (target: Discord.GuildMember, executor: Discord.GuildMember) => {
 
 const channel = (target: Discord.GuildBasedChannel, executor: Discord.GuildMember) =>
  executor.permissionsIn(target).has(Discord.PermissionFlagsBits.ManageChannels);
+
+const role = (target: Discord.Role, executor: Discord.GuildMember) => {
+ if (!executor.permissions.has(Discord.PermissionFlagsBits.ManageRoles)) return false;
+ if (target.position >= executor.roles.highest.position) return false;
+ return true;
+};
