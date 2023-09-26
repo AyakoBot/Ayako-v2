db83415a76d695de91dce6670fe270794cfc0075
Larsundso <Larsundso3@gmail.com>
2023-09-26 19:27:15 +0200

    remove kick

--- src/Commands/StringCommands/kick.ts
+++
@@ -1,5 +0,0 @@
-import * as Discord from 'discord.js';
-import * as ch from '../../BaseClient/ClientHelper.js';
-
-export const requiresSlashCommand = true;
-export default (msg: Discord.Message<true>) => ch.interactionHelpers(msg, msg.guild);
