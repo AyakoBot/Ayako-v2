147f4bab0499c7962c85b901b273e952bae8c995
Larsundso <Larsundso3@gmail.com>
2023-10-13 00:45:47 +0200

    reaction roles

--- src/Events/messageEvents/messageReactionRemove/messageReactionRemove.ts
+++ src/Events/messageEvents/messageReactionRemove/messageReactionRemove.ts
@@ -1,6 +1,7 @@
 import type * as Discord from 'discord.js';
 import log from './log.js';
 import * as ch from '../../../BaseClient/ClientHelper.js';
+import reactionRoles from './reactionRoles.js';
 
 export default async (reaction: Discord.MessageReaction, user: Discord.User) => {
  if (!reaction.message.guild) return;
@@ -11,4 +12,5 @@ export default async (reaction: Discord.MessageReaction, user: Discord.User) => 
  if (!msg) return;
 
  log(reaction, user, msg);
+ reactionRoles(reaction, user, msg);
 };
--- src/Events/messageEvents/messageReactionRemove/reactionRoles.ts
+++ src/Events/messageEvents/messageReactionRemove/reactionRoles.ts
@@ -16,20 +16,27 @@ export default async (
  });
  if (!reactionroles.length) return;
 
- const rolesToGive: string[] = [
-  ...reactionroles
-   .filter((r) => r.emote === decodeURI(reaction.emoji.identifier))
-   .map((r) => r.roles)
-   .flat(),
-  ...settings.anyroles,
- ];
- if (!rolesToGive.length) return;
+ const rolesToRemove: string[] = reactionroles
+  .filter((r) => r.emote === decodeURI(reaction.emoji.identifier))
+  .map((r) => r.roles)
+  .flat();
+ if (!rolesToRemove.length) return;
 
  const member = await ch.request.guilds
   .getMember(msg.guild, user.id)
   .then((r) => ('message' in r ? undefined : r));
  if (!member) return;
 
+ if (
+  !reactionroles
+   .map((r) => r.roles)
+   .flat()
+   .filter((r) => !rolesToRemove.includes(r))
+   .some((r) => member.roles.cache.has(r))
+ ) {
+  rolesToRemove.push(...settings.anyroles);
+ }
+
  const language = await ch.getLanguage(msg.guildId);
- ch.roleManager.add(member, rolesToGive, language.autotypes.reactionroles, 1);
+ ch.roleManager.remove(member, rolesToRemove, language.autotypes.reactionroles, 1);
 };
