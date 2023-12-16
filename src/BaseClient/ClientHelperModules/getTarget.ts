90df0f29fad5309b9a6ea311a3df56d79beb142d
Larsundso <Larsundso3@gmail.com>
2023-12-16 18:47:01 +0100

    minify and handle error

--- src/BaseClient/ClientHelperModules/getTarget.ts
+++ src/BaseClient/ClientHelperModules/getTarget.ts
@@ -4,28 +4,32 @@ import getReferenceMessage from './getReferenceMessage.js';
 import { request } from './requestHandler.js';
 import emotes from './emotes.js';
 import getUser from './getUser.js';
+import error from './error.js';
 
 export default async (msg: Discord.Message<true>, args: string[]) => {
  let user = msg.mentions.users.first();
 
- if (user?.id === (await getReferenceMessage(msg.reference))?.author.id) {
-  request.channels.addReaction(msg, constants.standard.getEmoteIdentifier(emotes.cross));
-  return undefined;
+ if (msg.reference && user?.id === (await getReferenceMessage(msg.reference))?.author.id) {
+  return react(msg);
  }
 
  if (!user) {
-  if (!args?.length) {
-   request.channels.addReaction(msg, constants.standard.getEmoteIdentifier(emotes.cross));
-   return undefined;
-  }
+  if (!args?.length) return react(msg);
 
   user = await getUser(args[0]);
  }
 
- if (!user) {
-  request.channels.addReaction(msg, constants.standard.getEmoteIdentifier(emotes.cross));
-  return undefined;
- }
+ if (!user) return react(msg);
 
  return user;
+};
+
+const react = async (msg: Discord.Message<true>) => {
+ const reaction = await request.channels.addReaction(
+  msg,
+  constants.standard.getEmoteIdentifier(emotes.cross),
+ );
+
+ if (typeof reaction !== 'undefined') error(msg.guild, new Error(reaction.message));
+ return undefined;
 };
