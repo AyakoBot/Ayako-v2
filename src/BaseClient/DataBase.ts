786f7f093f285fe6e5981f7d922b8ba5284c1a34
Larsundso <Larsundso3@gmail.com>
2023-12-27 18:22:57 +0100

    [PATCH] Optional REDIS

--- src/BaseClient/DataBase.ts
+++ src/BaseClient/DataBase.ts
@@ -4,33 +4,42 @@ import Redis from 'ioredis';
 
 const { log } = console;
 
-const redis = new Redis();
 const prisma = new PrismaClient();
 
-const options: Parameters<typeof createPrismaRedisCache>[0] = {
- storage: {
-  type: 'redis',
-  options: {
-   client: redis,
-   invalidation: {
-    referencesTTL: 300,
-   },
-   log: process.argv.includes('--debug-queries') ? console : undefined,
-  },
- },
- cacheTime: 300,
-};
+if (process.env.useRedis) {
+ const redis = new Redis();
 
-if (process.argv.includes('--debug-db')) {
- options.onHit = (key) => log(`[Redis] Cache Hit: ${key}`);
- options.onDedupe = (key) => log(`[Redis] Cache Dedupe: ${key}`);
- options.onMiss = (key) => log(`[Redis] Cache Miss: ${key}`);
- options.onError = (error) => log(`[Redis] Error: ${error}`);
+ const options: Parameters<typeof createPrismaRedisCache>[0] = {
+  storage: {
+   type: 'redis',
+   options: {
+    client: redis,
+    invalidation: {
+     referencesTTL: 300,
+    },
+    log: process.argv.includes('--debug-queries') ? console : undefined,
+   },
+  },
+  cacheTime: 300,
+ };
+
+ if (process.argv.includes('--debug-db')) {
+  options.onHit = (key) => log(`[Redis] Cache Hit: ${key}`);
+  options.onDedupe = (key) => log(`[Redis] Cache Dedupe: ${key}`);
+  options.onMiss = (key) => log(`[Redis] Cache Miss: ${key}`);
+  options.onError = (error) => log(`[Redis] Error: ${error}`);
+ }
+
+ const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache(options);
+
+ prisma.$use(cacheMiddleware);
+
+ redis.on('connect', () => log('[Redis] Connecting to Redis...'));
+ redis.on('ready', () => log('[Redis] Established Connection to DataBase'));
+ redis.on('error', (err) => log(`[Redis] Error: ${err}`));
+ redis.on('reconnecting', () => log('[Redis] Connection lost. Re-connecting...'));
 }
 
-const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache(options);
-
-prisma.$use(cacheMiddleware);
 prisma.$use(async (params, next) => {
  try {
   const result = await next(params);
@@ -43,10 +52,5 @@ prisma.$use(async (params, next) => {
   throw error;
  }
 });
-
-redis.on('connect', () => log('[Redis] Connecting to Redis...'));
-redis.on('ready', () => log('[Redis] Established Connection to DataBase'));
-redis.on('error', (err) => log(`[Redis] Error: ${err}`));
-redis.on('reconnecting', () => log('[Redis] Connection lost. Re-connecting...'));
 
 export default prisma;
