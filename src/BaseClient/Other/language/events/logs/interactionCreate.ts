e0cc273288bdc3cfe730dabc02ac0fc3a98dd270
Larsundso <Larsundso3@gmail.com>
2023-12-28 23:59:46 +0100

    [PATCH] Cooldown text fix

--- src/BaseClient/Other/language/events/logs/interactionCreate.ts
+++ src/BaseClient/Other/language/events/logs/interactionCreate.ts
@@ -2,5 +2,5 @@ import * as CT from '../../../../../Typings/Typings.js';
 
 export default (t: CT.Language) => ({
  ...t.JSON.events.interactionCreate,
- cooldown: (time: string) => t.stp(t.JSON.events.interactionCreate.cooldown, { time }),
+ cooldown: (time: string) => t.stp(t.JSON.events.interactionCreate.cooldown, { t: time }),
 });
