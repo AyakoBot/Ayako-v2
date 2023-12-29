35636149ec14efc53b4ee1261700c53c472f7698
Larsundso <Larsundso3@gmail.com>
2023-12-29 18:58:49 +0100

    fix text

--- src/BaseClient/Other/language/slashCommands/info/bot.ts
+++ src/BaseClient/Other/language/slashCommands/info/bot.ts
@@ -7,7 +7,7 @@ export default (t: CT.Language) => ({
  ...t.JSON.slashCommands.info.bot,
  author: t.stp(t.JSON.slashCommands.info.bot.author, { t }),
  base: t.stp(t.JSON.slashCommands.info.bot.base, {
-  base: t.botId === ch.mainID ? '' : `(${t.JSON.slashCommands.info.bot.thisBase})`,
+  base: t.botId === ch.mainID ? ' ' : `(${t.JSON.slashCommands.info.bot.thisBase}) `,
   version: packageJSON.version,
  }),
 });
