df3e1679fd96d50a67304782fec9fae10de4ed77
Larsundso <Larsundso3@gmail.com>
2023-12-29 01:06:41 +0100

    [PATCH] check invite perms before fetching

--- src/BaseClient/ClientHelperModules/requestHandler/guilds/getInvites.ts
+++ src/BaseClient/ClientHelperModules/requestHandler/guilds/getInvites.ts
@@ -4,13 +4,25 @@ import { API } from '../../../Client.js';
 import cache from '../../cache.js';
 import * as Classes from '../../../Other/classes.js';
 
+import getBotMemberFromGuild from '../../getBotMemberFromGuild.js';
+import requestHandlerError from '../../requestHandlerError.js';
+
 /**
  * Retrieves the invites for a given guild.
  * @param guild The guild to retrieve invites for.
  * @returns A promise that resolves with an array of parsed invite objects.
  */
-export default async (guild: Discord.Guild) =>
- (cache.apis.get(guild.id) ?? API).guilds
+export default async (guild: Discord.Guild) => {
+ if (
+  !(await getBotMemberFromGuild(guild)).permissions.has(Discord.PermissionFlagsBits.ManageGuild)
+ ) {
+  const e = requestHandlerError(`Cannot get invites`, [Discord.PermissionFlagsBits.ManageGuild]);
+
+  error(guild, e);
+  return e;
+ }
+
+ return (cache.apis.get(guild.id) ?? API).guilds
   .getInvites(guild.id)
   .then((invites) => {
    const parsed = invites.map((i) => new Classes.Invite(guild.client, i));
@@ -24,3 +36,12 @@ export default async (guild: Discord.Guild) =>
    error(guild, new Error((e as Discord.DiscordAPIError).message));
    return e as Discord.DiscordAPIError;
   });
+};
+/**
+ * Checks if the specified guild member has the permission to get guild invites.
+ * @param me - The guild member to check.
+ * @returns A promise that resolves to a boolean,
+ * indicating whether the guild member can get guild invites.
+ */
+export const canGetInvites = async (me: Discord.GuildMember) =>
+ me.permissions.has(Discord.PermissionFlagsBits.ManageGuild);
