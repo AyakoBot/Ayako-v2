5c8541c23777abb983be28bec490f2f6475b8bc7
Larsundso <Larsundso3@gmail.com>
2023-12-31 03:41:15 +0100

    fix author display

--- src/BaseClient/Other/language/slashCommands/leaderboard.ts
+++ src/BaseClient/Other/language/slashCommands/leaderboard.ts
@@ -8,4 +8,6 @@ export default (t: CT.Language) => ({
    msgs: ch.util.makeBold(ch.splitByThousand(msgs)),
    duration: ch.util.makeInlineCode(duration),
   }),
+ lleaderboard: t.stp(t.JSON.slashCommands.leaderboard.lleaderboard, { t }),
+ nleaderboard: t.stp(t.JSON.slashCommands.leaderboard.nleaderboard, { t }),
 });
