945a24d0e6c1d2c44b1b8768be764694f898f250
Larsundso <Larsundso3@gmail.com>
2023-12-31 03:40:51 +0100

    round

--- src/BaseClient/ClientHelperModules/splitByThousands.ts
+++ src/BaseClient/ClientHelperModules/splitByThousands.ts
@@ -4,7 +4,7 @@
  * @returns The number as a string with commas separating each thousands place.
  */
 const splitByThousand = (num: number): string => {
- const str = num.toString();
+ const str = Math.round(num).toString();
  const len = str.length;
  if (len <= 3) return str;
  return `${splitByThousand(parseInt(str.slice(0, len - 3), 10))},${str.slice(len - 3)}`;
