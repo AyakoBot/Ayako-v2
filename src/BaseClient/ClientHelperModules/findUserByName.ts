a8565cbb9c9f0a4a5c754894fc9293eb5e4295db
Larsundso <Larsundso3@gmail.com>
2024-01-01 21:03:58 +0100

    something here is null

--- src/BaseClient/ClientHelperModules/findUserByName.ts
+++ src/BaseClient/ClientHelperModules/findUserByName.ts
@@ -8,7 +8,7 @@ export default async (name: string) =>
   await (
    await import('../Client.js')
   ).default.shard?.broadcastEval(
-   (cl, { n }) => cl.users.cache.filter((u) => u.username.toLowerCase().includes(n.toLowerCase())),
+   (cl, { n }) => cl.users.cache.filter((u) => u.username?.toLowerCase().includes(n?.toLowerCase())),
    {
     context: { n: name },
    },
