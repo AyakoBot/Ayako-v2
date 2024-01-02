cf8cd4a4cdb09a3908c540da50766565d162d330
Larsundso <Larsundso3@gmail.com>
2024-01-02 02:30:09 +0100

    Improved Pinging

--- src/Commands/SlashCommands/ping.ts
+++ src/Commands/SlashCommands/ping.ts
@@ -6,11 +6,31 @@ export default async (cmd: Discord.ChatInputCommandInteraction) => {
  const language = await ch.getLanguage(cmd.guildId);
  const lan = language.slashCommands.ping;
 
- ch.replyCmd(cmd, {
+ const sent = await ch.replyCmd(cmd, {
   embeds: [
    {
     description: `**${lan.lastHeartbeat}**: ${ch.util.makeInlineCode(
      String(stats?.heartbeat ?? 0),
+    )} ${language.time.milliseconds}\n**${lan.websocket}**: ${ch.util.makeInlineCode(
+     String(cmd.client.ws.shards.get(Number(cmd.client.shard?.ids[0]))?.ping ?? 0),
+    )} ${language.time.milliseconds}`,
+    color: ch.getColor(cmd.guild ? await ch.getBotMemberFromGuild(cmd.guild) : undefined),
+    author: { name: lan.author },
+   },
+  ],
+ });
+
+ if (!sent) return;
+
+ cmd.editReply({
+  embeds: [
+   {
+    description: `**${lan.lastHeartbeat}**: ${ch.util.makeInlineCode(
+     String(stats?.heartbeat ?? 0),
+    )} ${language.time.milliseconds}\n**${lan.websocket}**: ${ch.util.makeInlineCode(
+     String(cmd.client.ws.shards.get(Number(cmd.client.shard?.ids[0]))?.ping ?? 0),
+    )} ${language.time.milliseconds}\n**${lan.roundtrip}**: ${ch.util.makeInlineCode(
+     String(sent.createdTimestamp - cmd.createdTimestamp),
     )} ${language.time.milliseconds}`,
     color: ch.getColor(cmd.guild ? await ch.getBotMemberFromGuild(cmd.guild) : undefined),
     author: { name: lan.author },
