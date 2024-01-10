1654993a65ef21a85449cb26f6bd9b939f596133
Larsundso <Larsundso3@gmail.com>
2024-01-10 18:23:31 +0100

    remove Websocket Latency

--- src/Commands/SlashCommands/ping.ts
+++ src/Commands/SlashCommands/ping.ts
@@ -10,8 +10,6 @@ export default async (cmd: Discord.ChatInputCommandInteraction) => {
    {
     description: `**${lan.lastHeartbeat}**: ${cmd.client.util.util.makeInlineCode(
      String(stats?.heartbeat ?? 0),
-    )} ${language.time.milliseconds}\n**${lan.websocket}**: ${cmd.client.util.util.makeInlineCode(
-     String(cmd.client.ws.shards.get(Number(cmd.client.shard?.ids[0]))?.ping ?? 0),
     )} ${language.time.milliseconds}`,
     color: cmd.client.util.getColor(
      cmd.guild ? await cmd.client.util.getBotMemberFromGuild(cmd.guild) : undefined,
@@ -28,8 +26,6 @@ export default async (cmd: Discord.ChatInputCommandInteraction) => {
    {
     description: `**${lan.lastHeartbeat}**: ${cmd.client.util.util.makeInlineCode(
      String(stats?.heartbeat ?? 0),
-    )} ${language.time.milliseconds}\n**${lan.websocket}**: ${cmd.client.util.util.makeInlineCode(
-     String(cmd.client.ws.shards.get(Number(cmd.client.shard?.ids[0]))?.ping ?? 0),
     )} ${language.time.milliseconds}\n**${lan.roundtrip}**: ${cmd.client.util.util.makeInlineCode(
      String(Math.abs(sent.createdTimestamp - Date.now())),
     )} ${language.time.milliseconds}`,
