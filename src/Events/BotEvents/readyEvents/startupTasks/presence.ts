374e6aef610ffbce5b827ba0d548518ac171f949
Larsundso <Larsundso3@gmail.com>
2024-01-16 15:34:00 +0100

    de-globalie for readability

--- src/Events/BotEvents/readyEvents/startupTasks/presence.ts
+++ src/Events/BotEvents/readyEvents/startupTasks/presence.ts
@@ -1,13 +1,12 @@
 import * as Discord from 'discord.js';
+import Sharding from 'discord-hybrid-sharding';
 import client from '../../../../BaseClient/Bot/Client.js';
 
 export default async () => {
  const getActivities = (thisShard: number): Discord.ActivitiesOptions => {
   const clusterShardsText = `Cluster ${Number(client.cluster?.id) + 1}/${
-   client.util.files.sharding.getInfo().CLUSTER_COUNT
-  } | Shard ${thisShard + 1}/${
-   client.util.files.sharding.getInfo().SHARD_LIST.length
-  } on this Cluster`;
+   Sharding.getInfo().CLUSTER_COUNT
+  } | Shard ${thisShard + 1}/${Sharding.getInfo().SHARD_LIST.length} on this Cluster`;
 
   return {
    name: 'Stable',
@@ -16,7 +15,7 @@ export default async () => {
   };
  };
 
- client.util.files.sharding.getInfo().SHARD_LIST.forEach((s) => {
+ Sharding.getInfo().SHARD_LIST.forEach((s) => {
   client.user?.setPresence({
    afk: false,
    activities: [getActivities(s)],
