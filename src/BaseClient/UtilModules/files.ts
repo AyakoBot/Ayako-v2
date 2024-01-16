718ab4107793c2843ca24e473c81944f2ab38d12
Larsundso <Larsundso3@gmail.com>
2024-01-16 15:25:16 +0100

    export discord and sharding packages

--- src/BaseClient/UtilModules/files.ts
+++ src/BaseClient/UtilModules/files.ts
@@ -1,4 +1,6 @@
 import { Prisma } from '@prisma/client';
+import * as Sharding from 'discord-hybrid-sharding';
+import * as Discord from 'discord.js';
 import * as Jobs from 'node-schedule';
 
 import * as endGiveaway from '../../Commands/SlashCommands/giveaway/end.js';
@@ -12,6 +14,8 @@ export default {
  // Packages
  prisma: Prisma,
  jobs: Jobs,
+ discord: Discord,
+ sharding: Sharding,
 
  // Files
  '/Typings/Typings.js': typings,
