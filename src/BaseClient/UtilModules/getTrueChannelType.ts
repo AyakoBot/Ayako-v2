23728cf5b0332d34d693058dbfd78097fb4ee94c
Larsundso <Larsundso3@gmail.com>
2024-01-20 03:15:25 +0100

    added channel type

--- src/BaseClient/UtilModules/getTrueChannelType.ts
+++ src/BaseClient/UtilModules/getTrueChannelType.ts
@@ -8,21 +8,17 @@ import * as Discord from 'discord.js';
  * @throws An error if the channel type is unknown.
  */
 export default (channel: Discord.Channel | Discord.GuildChannel, guild: Discord.Guild) => {
- switch (channel.type) {
+ switch (channel?.type) {
   case Discord.ChannelType.GuildText: {
    switch (true) {
-    case guild.rulesChannelId === channel.id: {
+    case guild.rulesChannelId === channel.id:
      return 'Rules';
-    }
-    case 'nsfw' in channel && channel.nsfw: {
+    case 'nsfw' in channel && channel.nsfw:
      return 'NSFWChannel';
-    }
-    case !!channel.permissionOverwrites: {
+    case !!channel.permissionOverwrites:
      return 'LockedChannel';
-    }
-    default: {
+    default:
      return 'TextChannel';
-    }
    }
   }
   case Discord.ChannelType.DM: {
@@ -30,46 +26,38 @@ export default (channel: Discord.Channel | Discord.GuildChannel, guild: Discord.
   }
   case Discord.ChannelType.GuildVoice: {
    switch (true) {
-    case !!channel.permissionOverwrites: {
+    case !!channel.permissionOverwrites:
      return 'LockedVoice';
-    }
-    default: {
+    default:
      return 'Voice';
-    }
    }
   }
-  case Discord.ChannelType.GroupDM: {
+  case Discord.ChannelType.GroupDM:
    return 'GroupDm';
-  }
-  case Discord.ChannelType.GuildCategory: {
+  case Discord.ChannelType.GuildCategory:
    return 'Category';
-  }
-  case Discord.ChannelType.GuildAnnouncement: {
+  case Discord.ChannelType.GuildAnnouncement:
    return 'NewsChannel';
-  }
   case Discord.ChannelType.AnnouncementThread:
   case Discord.ChannelType.PublicThread:
-  case Discord.ChannelType.PrivateThread: {
+  case Discord.ChannelType.PrivateThread:
    return 'Thread';
-  }
-  case Discord.ChannelType.GuildStageVoice: {
+  case Discord.ChannelType.GuildStageVoice:
    return 'Stage';
-  }
-  case Discord.ChannelType.GuildDirectory: {
+  case Discord.ChannelType.GuildDirectory:
    return 'Directory';
-  }
   case Discord.ChannelType.GuildForum: {
    switch (true) {
-    case 'nsfw' in channel && channel.nsfw: {
+    case 'nsfw' in channel && channel.nsfw:
      return 'NSFWForum';
-    }
-    default: {
+    default:
      return 'Forum';
-    }
    }
   }
-  default: {
+  case Discord.ChannelType.GuildMedia:
+   return 'MediaChannel';
+  default:
+   // @ts-ignore
    throw new Error(`Unknown Channel Type: ${channel.type} / ${Discord.ChannelType[channel.type]}`);
-  }
  }
 };
