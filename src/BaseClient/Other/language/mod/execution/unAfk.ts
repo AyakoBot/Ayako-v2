5f1773e1a6e12698496d3b442afa73d629dd8222
Larsundso <Larsundso3@gmail.com>
2024-01-24 11:18:48 +0100

    unafk => unAfk

--- src/BaseClient/Other/language/mod/execution/unAfk.ts
+++ src/BaseClient/Other/language/mod/execution/unAfk.ts
@@ -2,14 +2,14 @@ import * as Discord from 'discord.js';
 import * as CT from '../../../../../Typings/Typings.js';
 
 export default (t: CT.Language) => ({
- ...t.JSON.mod.execution.unafk,
- dm: () => t.JSON.mod.execution.unafk.dm,
+ ...t.JSON.mod.execution.unAfk,
+ dm: () => t.JSON.mod.execution.unAfk.dm,
  alreadyApplied: (target: Discord.User) =>
-  t.stp(t.JSON.mod.execution.unafk.alreadyApplied, {
+  t.stp(t.JSON.mod.execution.unAfk.alreadyApplied, {
    target: t.languageFunction.getUser(target),
   }),
  success: (target: Discord.User) =>
-  t.stp(t.JSON.mod.execution.unafk.success, {
+  t.stp(t.JSON.mod.execution.unAfk.success, {
    target: t.languageFunction.getUser(target),
   }),
 });
