dbdacefacdf16cd365bcdf3683c84df6b2661a93
Larsundso <Larsundso3@gmail.com>
2024-02-02 10:35:37 +0100

    importCache base file

---
+++ src/BaseClient/UtilModules/importCache.ts
@@ -0,0 +1,11 @@
+import BaseClient from './importCache/BaseClient.js';
+import pack from './importCache/package.js';
+import SlashCommands from './importCache/SlashCommands.js';
+import Typings from './importCache/Typings.js';
+
+export default {
+ BaseClient,
+ pack,
+ SlashCommands,
+ Typings,
+};
