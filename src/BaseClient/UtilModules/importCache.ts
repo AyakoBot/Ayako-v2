d64802cf32fb5ee3d2cdd065665236852de7a2a0
Larsundso <Larsundso3@gmail.com>
2024-02-04 00:21:00 +0100

    adding slash commands index

--- src/BaseClient/UtilModules/importCache.ts
+++ src/BaseClient/UtilModules/importCache.ts
@@ -1,15 +1,30 @@
 import BaseClient from './importCache/BaseClient.js';
 import Events from './importCache/Events.js';
-import SlashCommands from './importCache/SlashCommands.js';
 import Typings from './importCache/Typings.js';
 import pack from './importCache/package.js';
 import Language from './importCache/Language.js';
+import SlashCommandsIndex from './importCache/SlashCommands.js';
+
+import AutocompleteCommands from './importCache/Commands/AutocompleteCommands.js';
+import ButtonCommands from './importCache/Commands/ButtonCommands.js';
+import ContextCommands from './importCache/Commands/ContextCommands.js';
+import ModalCommands from './importCache/Commands/ModalCommands.js';
+import SelectCommands from './importCache/Commands/SelectCommands.js';
+import SlashCommands from './importCache/Commands/SlashCommands.js';
 
 export default {
  BaseClient,
  pack,
- SlashCommands,
  Typings,
  Events,
  Language,
+ SlashCommands: SlashCommandsIndex,
+ Commands: {
+  AutocompleteCommands,
+  ButtonCommands,
+  ContextCommands,
+  ModalCommands,
+  SelectCommands,
+  SlashCommands,
+ },
 };
