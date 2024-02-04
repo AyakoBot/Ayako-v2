7755cb9106fda6302e1f3a41715acfc1e12fd5d4
Larsundso <Larsundso3@gmail.com>
2024-02-04 23:51:42 +0100

    baseEvents fin

--- src/Events/BotEvents/shardReady.ts
+++ src/Events/BotEvents/shardReady.ts
@@ -1,8 +1,8 @@
 import log from '../../BaseClient/UtilModules/logError.js';
-import Presence from '../../BaseClient/Bot/Presence.js';
 import client from '../../BaseClient/Bot/Client.js';
 
-export default (id: number, unavailableGuilds?: Set<string>) => {
+export default async (id: number, unavailableGuilds?: Set<string>) => {
  log(`[Shard ${id + 1}] Ready - Unavailable Guilds: ${unavailableGuilds?.size ?? '0'}`, true);
- Presence(client.cluster!);
+
+ client.util.importCache.BaseClient.Bot.Presence.file.default(client.cluster!);
 };
