4c30fde50d16fd5c0d0a3f0814dc9d344cc44693
Larsundso <Larsundso3@gmail.com>
2024-02-06 12:32:46 +0100

    spammer cleaning am

---
+++ src/Events/BotEvents/readyEvents/timedFiles/amQuarantineControl.ts
@@ -0,0 +1,27 @@
+import * as Discord from 'discord.js';
+import client from '../../../../BaseClient/Bot/Client.js';
+import * as CT from '../../../../Typings/Typings.js';
+
+export default () => {
+ const guild = client.guilds.cache.get('298954459172700181');
+ if (!guild) return;
+
+ const members = guild.members.cache.filter((m) => m.user.flags?.has(Discord.UserFlags.Spammer));
+
+ members.forEach((m) => {
+  client.util.mod(
+   undefined,
+   CT.ModTypes.BanAdd,
+   {
+    dbOnly: false,
+    deleteMessageSeconds: 0,
+    executor: guild.client.user,
+    guild,
+    reason: 'Suspected Spammer',
+    target: m.user,
+    skipChecks: false,
+   },
+   undefined,
+  );
+ });
+};
