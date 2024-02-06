84fa14c9e292e332ce6e2f686ed81c9b32e1866a
Larsundso <Larsundso3@gmail.com>
2024-02-06 23:47:45 +0100

    file fix

--- src/BaseClient/UtilModules/importCache.ts
+++ src/BaseClient/UtilModules/importCache.ts
@@ -3,9 +3,9 @@ const self = {
   reload: async () => {
    self.package.file = await import(`../../../package.json?nonce=${Date.now()}`, {
     assert: { type: 'json' },
-   });
+   }).then((f) => f.default);
   },
-  file: await import('../../../package.json', { assert: { type: 'json' } }),
+  file: await import('../../../package.json', { assert: { type: 'json' } }).then((f) => f.default),
  },
  execute: {
   reload: async () => {
