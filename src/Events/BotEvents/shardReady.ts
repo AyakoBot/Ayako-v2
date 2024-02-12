ba9c91245ad6a2928e76c78d8623a86700f7208e
Larsundso <Larsundso3@gmail.com>
2024-02-12 22:32:39 +0100

    put setPresence in shardReady for less confusion

--- src/Events/BotEvents/shardReady.ts
+++ src/Events/BotEvents/shardReady.ts
@@ -1,5 +1,32 @@
+import * as Sharding from 'discord-hybrid-sharding';
+import * as Discord from 'discord.js';
+import client from '../../BaseClient/Bot/Client.js';
 import log from '../../BaseClient/UtilModules/logError.js';
 
-export default (id: number, unavailableGuilds?: Set<string>) => {
+export default async (id: number, unavailableGuilds?: Set<string>) => {
  log(`[Shard ${id + 1}] Ready - Unavailable Guilds: ${unavailableGuilds?.size ?? '0'}`, true);
+ client.user?.setPresence({
+  afk: false,
+  activities: [await getActivities(id)],
+  status: Discord.PresenceUpdateStatus.Online,
+  shardId: id,
+ });
+};
+
+const getActivities = async (thisShard: number): Promise<Discord.ActivitiesOptions> => {
+ const allShards = (
+  await client.cluster?.broadcastEval((cl) => cl.util.files.sharding.getInfo().SHARD_LIST)
+ )?.flat();
+
+ const clusterShardsText = `Shard ${thisShard + 1}/${
+  Sharding.getInfo().SHARD_LIST.length
+ } (Cluster)/${allShards?.length ?? 1} (Total) | Cluster ${Number(client.cluster?.id) + 1}/${
+  Sharding.getInfo().CLUSTER_COUNT
+ }`;
+
+ return {
+  name: 'Stable',
+  state: `/help | ${clusterShardsText}`,
+  type: Discord.ActivityType.Custom,
+ };
 };
