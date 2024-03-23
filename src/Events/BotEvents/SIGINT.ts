c7f616c21efb8adfe7b8300a0531b27029841d0a
Larsundso <Larsundso3@gmail.com>
2024-03-23 15:54:18 +0100

    graceful Jobs shutdown

--- src/Events/BotEvents/SIGINT.ts
+++ src/Events/BotEvents/SIGINT.ts
@@ -1,11 +1,13 @@
-import prisma from '../../BaseClient/Bot/DataBase.js';
+import * as Jobs from 'node-schedule';
 import client from '../../BaseClient/Bot/Client.js';
+import prisma from '../../BaseClient/Bot/DataBase.js';
 
 export default async () => {
  // eslint-disable-next-line no-console
  console.log('SIGINT detected, exiting...');
  await prisma.$disconnect();
  await client.destroy();
+ await Jobs.gracefulShutdown();
 
  process.exit(0);
 };
