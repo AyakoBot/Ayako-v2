c5b089a19e4ad21888a4332ac3dccb32e8b9517c
Larsundso <Larsundso3@gmail.com>
2024-03-28 15:32:10 +0100

    slower and more quietly

--- src/Events/BotEvents/readyEvents/timedFiles/reviews.ts
+++ src/Events/BotEvents/readyEvents/timedFiles/reviews.ts
@@ -9,59 +9,72 @@ let browser: Awaited<ReturnType<VanillaPuppeteer['launch']>> | null = null;
 const getContentFromURL = async (url: string) => {
  const page = await browser!.newPage();
  await page.goto(url);
- client.util.sleep(2000);
- const pageSourceHTML: string = await page.content();
+ await client.util.sleep(2000);
+ const pageSourceHTML = await page.content();
  page.close();
 
  return pageSourceHTML;
 };
 
-const getVersion = async () => {
- const content = await getContentFromURL('https://top.gg');
+const extractJSON = (content: string) => JSON.parse(content.split('<pre>')[1].split('</pre>')[0]);
 
- return content
+const getVersion = async () =>
+ (await getContentFromURL('https://top.gg'))
   .split('https://cdn.top.gg/builds/_next/static/')
   .filter((c) => c.includes('_buildManifest.js'))[0]
   .split('/_buildManifest.js')[0];
-};
 
-const getPages = async () =>
+const getPages = async (version: string) =>
  Math.ceil(
   (
-   JSON.parse(
+   extractJSON(
     await getContentFromURL(
-     `https://top.gg/_next/data/${await getVersion()}/en/bot/${process.env.mainID}.json`,
+     `https://top.gg/_next/data/${version}/en/bot/${process.env.mainID}.json`,
     ),
    ) as CT.TopGGBotPage
   ).pageProps.reviewStats.reviewCount / 20,
  );
 
-const getAllReviews = async () =>
- Promise.all(
-  new Array(await getPages())
-   .fill(null)
-   .map((_, i) =>
-    getContentFromURL(
+const getAllReviews = async (version: string) => {
+ const reviews: CT.TopGGReview[][] = [];
+ const pages = await getPages(version);
+
+ for (let i = 0; i < pages; i += 1) {
+  reviews.push(
+   extractJSON(
+    // eslint-disable-next-line no-await-in-loop
+    await getContentFromURL(
      `https://top.gg/api/client/entities/${process.env.mainID}/reviews?page=${i + 1}`,
-    ).then((r) => JSON.parse(r) as CT.TopGGReview[]),
-   ),
+    ),
+   ) as CT.TopGGReview[],
+  );
+ }
+
+ return reviews.flat();
+};
+
+const getUser = async (userId: string, version: string) =>
+ extractJSON(
+  await getContentFromURL(`https://top.gg/_next/data/${version}/en/user/${userId}.json`),
  );
 
-const getUser = async (userId: string): Promise<CT.TopGGUserPage> =>
- JSON.parse(
-  await getContentFromURL(`https://top.gg/_next/data/${await getVersion()}/en/user/${userId}.json`),
- ) as CT.TopGGUserPage;
-
 export default async () => {
- browser = await puppeteer.launch({ headless: true });
+ browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
 
  await client.util.DataBase.reviews.deleteMany({
   where: { fetchat: { lt: Date.now() - 60000 } },
  });
 
+ const version = await getVersion();
  const oldReviews = await client.util.DataBase.reviews.findMany({ select: { userid: true } });
- const allReviews = await getAllReviews();
- const realUsers = await Promise.all(allReviews.flat().map((r) => getUser(r.posterId)));
+ const allReviews = await getAllReviews(version);
+
+ const realUsers: CT.TopGGUserPage[] = [];
+ for (let i = 0; i < allReviews.length; i += 1) {
+  // eslint-disable-next-line no-await-in-loop
+  realUsers.push((await getUser(allReviews[i].posterId, version)) as CT.TopGGUserPage);
+ }
+
  const newReviews = allReviews
   .flat()
   .filter(
