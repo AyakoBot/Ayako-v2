c25ff134e66668eed3e27158e91a14b1c40091f6
Larsundso <Larsundso3@gmail.com>
2024-05-23 17:30:46 +0200

    sticker emote stats

--- src/Events/BotEvents/readyEvents/timedFiles/stats.ts
+++ src/Events/BotEvents/readyEvents/timedFiles/stats.ts
@@ -1,20 +1,27 @@
 import client from '../../../../BaseClient/Bot/Client.js';
 
 export default async () => {
- const [userSize, guildSize, memberSize, roleSize, channelSize] = await Promise.all([
-  client.cluster?.fetchClientValues('users.cache.size'),
-  client.cluster?.fetchClientValues('guilds.cache.size'),
-  client.cluster?.broadcastEval((c) =>
-   c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0),
-  ),
-  client.cluster?.broadcastEval((c) =>
-   c.guilds.cache.reduce((acc, guild) => acc + guild.roles.cache.size, 0),
-  ),
-  client.cluster?.broadcastEval((c) =>
-   c.guilds.cache.reduce((acc, guild) => acc + guild.channels.cache.size, 0),
-  ),
- ]);
+ const [emojiSize, userSize, guildSize, memberSize, roleSize, channelSize, stickerSize] =
+  await Promise.all([
+   client.cluster?.fetchClientValues('emojis.cache.size'),
+   client.cluster?.fetchClientValues('users.cache.size'),
+   client.cluster?.fetchClientValues('guilds.cache.size'),
+   client.cluster?.broadcastEval((c) =>
+    c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0),
+   ),
+   client.cluster?.broadcastEval((c) =>
+    c.guilds.cache.reduce((acc, guild) => acc + guild.roles.cache.size, 0),
+   ),
+   client.cluster?.broadcastEval((c) =>
+    c.guilds.cache.reduce((acc, guild) => acc + guild.channels.cache.size, 0),
+   ),
+   client.cluster?.broadcastEval((c) =>
+    c.guilds.cache.reduce((acc, guild) => acc + guild.stickers.cache.size, 0),
+   ),
+  ]);
 
+ const totalemotes =
+  (emojiSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
  const totalusers = (userSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
  const totalmembers =
   (memberSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
@@ -24,6 +31,8 @@ export default async () => {
   (channelSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
  const totalguildcount =
   (guildSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
+ const totalstickercount =
+  (stickerSize as number[] | undefined)?.reduce((acc, count) => acc + count, 0) ?? 0;
 
  client.util.DataBase.stats
   .updateMany({
@@ -33,6 +42,8 @@ export default async () => {
     channelcount: totalchannelcount,
     rolecount: totalrolecount,
     allusers: totalmembers,
+    emotecount: totalemotes,
+    stickercount: totalstickercount,
    },
   })
   .then();
