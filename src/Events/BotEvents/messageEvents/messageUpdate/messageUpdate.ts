80fdaf0cce028cb9d480090d75169b965bb12c0f
Larsundso <Larsundso3@gmail.com>
2024-05-23 17:15:35 +0200

    edit dm log

--- src/Events/BotEvents/messageEvents/messageUpdate/messageUpdate.ts
+++ src/Events/BotEvents/messageEvents/messageUpdate/messageUpdate.ts
@@ -1,9 +1,14 @@
 import type * as Discord from 'discord.js';
+
 import commandHandler from '../messageCreate/commandHandler.js';
+import dmLog from '../messageCreate/dmLog.js';
 import log from './log.js';
 
 export default async (oldMsg: Discord.Message, msg: Discord.Message) => {
- if (!msg.inGuild()) return;
+ if (!msg.inGuild()) {
+  dmLog(msg);
+  return;
+ }
 
  log(oldMsg, msg);
  commandHandler(msg);
