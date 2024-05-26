019fed9286003869c842a38267e0b2d6749336a3
Larsundso <Larsundso3@gmail.com>
2024-05-26 12:21:23 +0200

    return on dev

--- src/Events/BotEvents/readyEvents/timedFiles/guilds.ts
+++ src/Events/BotEvents/readyEvents/timedFiles/guilds.ts
@@ -1,6 +1,8 @@
 import client from '../../../../BaseClient/Bot/Client.js';
 
 export default async () => {
+ if (client.user?.id !== process.env.mainId) return;
+
  await client.util.DataBase.guilds.deleteMany({ where: { fetchat: { lt: Date.now() - 60000 } } });
 
  client.guilds.cache.forEach((g) => {
