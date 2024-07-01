b9faf78c77dba1194212e86a47405ab532a8d452
Larsundso <Larsundso3@gmail.com>
2024-07-01 11:57:56 +0200

    botlist.me

---
+++ src/BaseClient/Cluster/Stats/BotList.ts
@@ -0,0 +1,17 @@
+import Manager from '../Manager.js';
+
+const api = 'https://api.botlist.me/api/v1/bots/650691698409734151/stats';
+
+export default (guilds: number) => {
+ fetch(api, {
+  method: 'post',
+  headers: {
+   Authorization: process.env.botListToken ?? '',
+   'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+   server_count: guilds,
+   shard_count: Manager.totalShards,
+  }),
+ }).then(async (r) => (r.ok ? undefined : console.log(await r.text())));
+};
