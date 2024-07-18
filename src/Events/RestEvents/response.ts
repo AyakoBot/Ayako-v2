3287a1a114814585d1ea1484c9c6be83d3fa64df
Larsundso <Larsundso3@gmail.com>
2024-07-18 13:49:13 +0200

    method

--- src/Events/RestEvents/response.ts
+++ src/Events/RestEvents/response.ts
@@ -4,6 +4,6 @@ import { getDebugInfo } from '../../BaseClient/UtilModules/console.js';
 
 export default async (request: Discord.APIRequest, response: Discord.ResponseLike) => {
  client.util.logFiles.ratelimits.write(
-  `${getDebugInfo()} [Request] ${request.path} - [Response] ${response.status} | ${response.statusText}\n`,
+  `${getDebugInfo()} [Request] ${request.method} ${request.path} - [Response] ${response.status} | ${response.statusText}\n`,
  );
 };
