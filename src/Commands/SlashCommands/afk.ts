35a91a7b6358aa948851f7b54fb432faf35763d0
Larsundso <Larsundso3@gmail.com>
2024-08-07 21:42:33 +0200

    debuggin

--- src/Commands/SlashCommands/afk.ts
+++ src/Commands/SlashCommands/afk.ts
@@ -10,13 +10,19 @@ export default async (
  if (!cmd.inGuild()) return;
  if (!cmd.channel) return;
 
+ if (cmd.guildId === '672546390915940405') console.log(1);
+
  const language = await cmd.client.util.getLanguage(cmd.guildId);
  const lan = language.slashCommands.afk;
  const author = cmd instanceof Discord.ChatInputCommandInteraction ? cmd.user : cmd.author;
 
+ if (cmd.guildId === '672546390915940405') console.log(2);
+
  const afk = await cmd.client.util.DataBase.afk.findUnique({
   where: { userid_guildid: { userid: author.id, guildid: cmd.guildId } },
  });
+
+ if (cmd.guildId === '672546390915940405') console.log(3);
 
  if (cmd instanceof Discord.ChatInputCommandInteraction) {
   text = cmd.options.getString('reason', false) ?? undefined;
@@ -30,6 +36,8 @@ export default async (
      },
     ]
   : [];
+
+ if (cmd.guildId === '672546390915940405') console.log(4);
 
  if (cmd instanceof Discord.ChatInputCommandInteraction && !afk) {
   await cmd.client.util.replyCmd(cmd, {
@@ -52,6 +60,8 @@ export default async (
   if (await cmd.client.util.isDeleteable(cmd)) cmd.client.util.request.channels.deleteMessage(cmd);
  }
 
+ if (cmd.guildId === '672546390915940405') console.log(5);
+
  if (
   cmd.member &&
   cmd.member.id !== cmd.guild.ownerId &&
@@ -64,6 +74,8 @@ export default async (
    lan.setReason,
   );
  }
+
+ if (cmd.guildId === '672546390915940405') console.log(6);
 
  cmd.client.util.DataBase.afk
   .upsert({
