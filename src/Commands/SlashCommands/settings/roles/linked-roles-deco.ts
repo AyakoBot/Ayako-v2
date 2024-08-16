e4ab3b108dfa2fe170fe3c637919762b5bc9d79d
Larsundso <Larsundso3@gmail.com>
2024-08-16 21:23:36 +0200

    fix #204

--- src/Commands/SlashCommands/settings/roles/linked-roles-deco.ts
+++ src/Commands/SlashCommands/settings/roles/linked-roles-deco.ts
@@ -154,6 +154,7 @@ export const getComponents: CT.SettingsFile<typeof name>['getComponents'] = (
  {
   type: Discord.ComponentType.ActionRow,
   components: [
+   buttonParsers.delete(language, name, Number(settings?.uniquetimestamp)),
    buttonParsers.specific(
     language,
     settings?.roleId,
