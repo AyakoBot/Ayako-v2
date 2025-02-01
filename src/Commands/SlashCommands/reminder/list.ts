fb229af1838cc14c0563eef95a5a9739e4784e6b
Larsundso <Larsundso3@gmail.com>
2025-02-01 10:37:42 +0100

    website compatible

--- src/Commands/SlashCommands/reminder/list.ts
+++ src/Commands/SlashCommands/reminder/list.ts
@@ -1,5 +1,7 @@
 import * as Discord from 'discord.js';
 
+const isSnowflake = (id: string) => /^\d{17,19}$/.test(id);
+
 export default async (cmd: Discord.ChatInputCommandInteraction) => {
  const language = await cmd.client.util.getLanguage(cmd.guildId);
  const lan = language.slashCommands.reminder;
@@ -10,7 +12,7 @@ export default async (cmd: Discord.ChatInputCommandInteraction) => {
  const embed: Discord.APIEmbed = {
   description: lan.desc((await cmd.client.util.getCustomCommand(cmd.guild, 'reminder'))?.id ?? '0'),
   fields: reminders.map((r) => ({
-   name: `<#${r.channelid}> | ${cmd.client.util.constants.standard.getTime(
+   name: `${isSnowflake(r.channelid) ? `<#${r.channelid}>` : language.t.Website} | ${cmd.client.util.constants.standard.getTime(
     Number(r.endtime),
    )} | ID: \`${Number(r.uniquetimestamp).toString(36)}\``,
    value: r.reason,
