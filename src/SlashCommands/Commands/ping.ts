81ebdb5e9e9322a863c533c40f4dbcae3383645d
Larsundso <Larsundso3@gmail.com>
2025-04-09 20:50:14 +0200

    redudant "the"

--- src/SlashCommands/Commands/ping.ts
+++ src/SlashCommands/Commands/ping.ts
@@ -2,7 +2,7 @@ import * as Discord from 'discord.js';
 
 export default new Discord.SlashCommandBuilder()
  .setName('ping')
- .setDescription(`Display the ${process.env.mainName}'s Ping`)
+ .setDescription(`Display ${process.env.mainName}'s Ping`)
  .setContexts([
   Discord.InteractionContextType.BotDM,
   Discord.InteractionContextType.Guild,
