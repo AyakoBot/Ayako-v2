a08d193d28cdc50d496f4a70061d71a21daa09c9
Larsundso <Larsundso3@gmail.com>
2025-05-10 19:39:39 +0200

    fix #336

--- src/BaseClient/Other/language/mod/logs/vcTempDeafenAdd.ts
+++ src/BaseClient/Other/language/mod/logs/vcTempDeafenAdd.ts
@@ -3,9 +3,14 @@ import * as CT from '../../../../../Typings/Typings.js';
 
 export default (t: CT.Language) => ({
  ...t.JSON.mod.logs.vcTempDeafenAdd,
- description: (target: Discord.User, executor: Discord.User) =>
+ description: (
+  target: Discord.User,
+  executor: Discord.User,
+  options: CT.ModOptions<CT.ModTypes.VcTempDeafenAdd>,
+ ) =>
   t.stp(t.JSON.mod.logs.vcTempDeafenAdd.description, {
    target: t.languageFunction.getUser(target),
    executor: t.languageFunction.getUser(executor),
+   duration: t.util.constants.standard.getTime(Date.now() + options.duration * 1000),
   }),
 });
--- src/BaseClient/Other/language/mod/logs/vcTempMuteAdd.ts
+++ src/BaseClient/Other/language/mod/logs/vcTempMuteAdd.ts
@@ -3,9 +3,14 @@ import * as CT from '../../../../../Typings/Typings.js';
 
 export default (t: CT.Language) => ({
  ...t.JSON.mod.logs.vcTempMuteAdd,
- description: (target: Discord.User, executor: Discord.User) =>
+ description: (
+  target: Discord.User,
+  executor: Discord.User,
+  options: CT.ModOptions<CT.ModTypes.VcTempMuteAdd>,
+ ) =>
   t.stp(t.JSON.mod.logs.vcTempMuteAdd.description, {
    target: t.languageFunction.getUser(target),
    executor: t.languageFunction.getUser(executor),
+   duration: t.util.constants.standard.getTime(Date.now() + options.duration * 1000),
   }),
 });
