81a1a1486a014a1bfef3ac588012a7166d47b03c
Larsundso <Larsundso3@gmail.com>
2025-06-19 19:19:39 +0200

    improved qualification checking, related #360

--- src/Events/BotEvents/guildEvents/guildMemberUpdate/linkedRolesDeco.ts
+++ src/Events/BotEvents/guildEvents/guildMemberUpdate/linkedRolesDeco.ts
@@ -1,14 +1,21 @@
 import type { GuildMember, Role } from 'discord.js';
 import { revokeToken } from '../../../../Commands/SlashCommands/settings/roles/linked-roles-deco.js';
+import linkedRolesDeco from './linkedRolesDeco.js';
 
-export default async (member: GuildMember, roleOverwrite: Role[] | null = null) => {
+export default async (
+ member: GuildMember,
+ roleOverwrite: Role[] | null = null,
+ verificationRun: boolean = false,
+) => {
  const settings = await member.client.util.DataBase.linkedRolesDeco.findMany({
   where: { guildid: member.guild.id },
  });
  if (!settings) return;
 
  const notQualified = settings.filter(
-  (s) => !(roleOverwrite || member.roles.cache).some((r) => s.allowedRoles.includes(r.id)),
+  (s) =>
+   !(roleOverwrite || member.roles.cache).some((r) => s.allowedRoles.includes(r.id)) &&
+   !s.allowedUsers.includes(member.id),
  );
  if (!notQualified.length) return;
 
@@ -18,6 +25,18 @@ export default async (member: GuildMember, roleOverwrite: Role[] | null = null) 
   })
   .then((r) => r.map((t) => ({ ...t, settings: notQualified.find((s) => s.botId === t.botId) })));
  if (!tokens.length) return;
+
+ if (!verificationRun) {
+  const fetched = await member.client.util.request.guilds.getMember(member.guild, member.id);
+
+  if (!fetched || 'message' in fetched) {
+   linkedRolesDeco(member, [], true);
+   return;
+  }
+
+  linkedRolesDeco(fetched, roleOverwrite, true);
+  return;
+ }
 
  tokens
   .filter((t) => !!t.settings?.botSecret)
