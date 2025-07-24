fbab7670b06ef7309018838bb2191539947d5eb9
Larsundso <Larsundso3@gmail.com>
2025-07-25 00:51:38 +0200

    fix: primary key mismatch

--- src/Events/BotEvents/messageEvents/messageDelete/welcomeGifChannel.ts
+++ src/Events/BotEvents/messageEvents/messageDelete/welcomeGifChannel.ts
@@ -1,5 +1,5 @@
 import type { Message } from 'discord.js';
 
 export default async (msg: Message<true>) => {
- msg.client.util.DataBase.welcomeGIF.delete({ where: { msgId: msg.id } }).then();
+ msg.client.util.DataBase.welcomeGIF.deleteMany({ where: { msgId: msg.id } }).then();
 };
