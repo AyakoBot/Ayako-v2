f65b3d342a2e4aafb30d151e9ccbc04737ad9736
Larsundso <Larsundso3@gmail.com>
2025-08-10 18:22:42 +0200

    treat updates like sends

--- src/Events/BotEvents/messageEvents/messageUpdate/messageUpdate.ts
+++ src/Events/BotEvents/messageEvents/messageUpdate/messageUpdate.ts
@@ -3,12 +3,19 @@ import type * as Discord from 'discord.js';
 import commandHandler from '../messageCreate/commandHandler.js';
 import dmLog from '../messageCreate/dmLog.js';
 import log from './log.js';
+import newlines from '../messageCreate/newlines.js';
+import invites from '../messageCreate/invites.js';
+import antivirus from '../messageCreate/antivirus.js';
 
 export default async (oldMsg: Discord.Message, msg: Discord.Message) => {
  if (!msg.inGuild()) {
   dmLog(msg);
   return;
  }
+
+ newlines(msg);
+ invites(msg);
+ antivirus(msg);
 
  log(oldMsg, msg);
  commandHandler(msg);
